3e8a1a5cd93c6679a0682886b9f4c667eff7d684
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-08-10 12:26:40 +0530

    👍 Added script to get info about downloaded albums

---
+++ lib/scripts/savedalbums.dart
@@ -0,0 +1,67 @@
+library savedalbums;
+
+import 'dart:io';
+import 'dart:convert' as convert;
+import 'package:path_provider/path_provider.dart' as path;
+import 'package:path/path.dart' as path;
+
+class SavedAlbums {
+
+  static Future<Map<String, dynamic>> albums() async {
+
+    Directory externalDirectory = (await path.getExternalStorageDirectory());
+    Directory applicationDirectory = Directory(path.join(externalDirectory.path, '.harmonoid'));
+    Directory musicDirectory = Directory(path.join(applicationDirectory.path, 'musicLibrary'));
+
+    if (!(await applicationDirectory.exists())) {
+      await musicDirectory.create();
+    }
+
+    Map<String, dynamic> albums = {'albums': ''};
+
+    List<FileSystemEntity> savedAlbumsFolder = musicDirectory.listSync();
+    List<Map<String, dynamic>> savedAlbums = new List<Map<String, dynamic>>();
+
+    for (Directory file in savedAlbumsFolder) {
+      File albumAssetsFile = File(path.join(file.path, 'albumAssets.json'));
+      Map<String, dynamic> album = convert.jsonDecode(await albumAssetsFile.readAsString());
+      savedAlbums.add(album);
+    }
+    albums['albums'] = savedAlbums;
+
+    return albums;
+  }
+
+  static Future<Map<String, dynamic>> tracks(String albumId) async {
+
+    Directory externalDirectory = (await path.getExternalStorageDirectory());
+    Directory applicationDirectory = Directory(path.join(externalDirectory.path, '.harmonoid'));
+    Directory musicDirectory = Directory(path.join(applicationDirectory.path, 'musicLibrary'));
+
+    File albumAssetsFile = File(path.join(musicDirectory.path, albumId, 'trackAssets.json'));
+    Map<String, dynamic> tracks = convert.jsonDecode(await albumAssetsFile.readAsString());
+    return tracks;
+  }
+
+  static Future<List<File>> albumArts() async {
+
+    Directory externalDirectory = (await path.getExternalStorageDirectory());
+    Directory applicationDirectory = Directory(path.join(externalDirectory.path, '.harmonoid'));
+    Directory musicDirectory = Directory(path.join(applicationDirectory.path, 'musicLibrary'));
+
+    if (!(await applicationDirectory.exists())) {
+      await musicDirectory.create();
+    }
+
+    List<File> albumArts = new List<File>();
+
+    List<FileSystemEntity> savedAlbumsFolder = musicDirectory.listSync();
+
+    for (Directory file in savedAlbumsFolder) {
+      File albumArt = File(path.join(file.path, 'albumArt.png'));
+      albumArts.add(albumArt);
+    }
+
+    return albumArts;
+  }
+}
