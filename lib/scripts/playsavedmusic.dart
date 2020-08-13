e51423f17392a8554ae9cb7227d0220c2ef04e85
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-08-13 19:31:46 +0530

    🔧 Updated static function to make use of background service for playback

--- lib/scripts/playsavedmusic.dart
+++ lib/scripts/playsavedmusic.dart
@@ -1,9 +1,13 @@
 library playsavedmusic;
 
+import 'package:audio_service/audio_service.dart';
+
 import 'dart:io';
-import 'package:audioplayers/audioplayers.dart';
+import 'dart:convert' as convert;
 import 'package:path_provider/path_provider.dart' as path;
 import 'package:path/path.dart' as path;
+
+import 'package:harmonoid/scripts/getsavedmusic.dart';
 
 class PlaySavedMusic {
   static Future<void> playTrack(String albumId, int trackNumber) async {
@@ -12,9 +16,32 @@ class PlaySavedMusic {
     Directory applicationDirectory = Directory(path.join(externalDirectory.path, '.harmonoid'));
     Directory musicDirectory = Directory(path.join(applicationDirectory.path, 'musicLibrary'));
 
-    String trackPath = path.join(musicDirectory.path, albumId, '$trackNumber.m4a');
+    File albumAssetsFile = File(path.join(musicDirectory.path, albumId, 'albumAssets.json'));
 
-    AudioPlayer audioPlayer = new AudioPlayer();
-    await audioPlayer.play(trackPath, isLocal: true);
+    Map<String, dynamic> albumAssets = convert.jsonDecode(await albumAssetsFile.readAsString());
+
+    List<Map<String, dynamic>> albumTracks = (await GetSavedMusic.tracks(albumId))['tracks'];
+    List<MediaItem> trackQueue = new List<MediaItem>();
+
+    for (int index = 0; index < albumTracks.length; index++) {
+      trackQueue.add(
+        MediaItem(
+          id: albumAssets['album_id'] + '_' + albumTracks[index]['track_number'].toString(),
+          title: albumTracks[index]['track_name'].split('(')[0].trim().split('-')[0].trim(),
+          album: albumAssets['album_name'].split('(')[0].trim().split('-')[0].trim(),
+          artist: albumAssets['album_artists'][0],
+          artUri: 'file://${path.join(musicDirectory.path, albumId, 'albumArt.png')}',
+          extras: {
+            'album_id': albumAssets['album_id'], 
+            'track_id': albumTracks[index]['track_id'],
+            'track_number': albumTracks[index]['track_number'],
+            'track_path': path.join(musicDirectory.path, albumId, '${albumTracks[index]['track_number']}.m4a')
+          },
+        ),
+      );
+    }
+
+    await AudioService.updateQueue(trackQueue);
+    await AudioService.playFromMediaId(albumAssets['album_id'] + '_' + trackNumber.toString());
   }
 }
