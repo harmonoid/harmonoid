c0a24dffddf03dfa77229e3260a10d03349eebe9
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-08-16 21:17:15 +0530

    👌 Added static methods for search history handling

---
+++ lib/scripts/searchhistory.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+import 'dart:convert' as convert;
+import 'package:path_provider/path_provider.dart' as path;
+import 'package:path/path.dart' as path;
+
+
+class SearchHistory {
+
+  static Future<Map<String, dynamic>> getSearchHistory() async {
+    Map<String, dynamic> searchHistory;
+    try {
+      Directory externalDirectory = (await path.getExternalStorageDirectory());
+      Directory applicationDirectory = Directory(path.join(externalDirectory.path, '.harmonoid'));
+
+      File searchHistoryFile = File(path.join(applicationDirectory.path, 'search.json'));
+
+      if (await searchHistoryFile.exists()) {
+        searchHistory = convert.jsonDecode(await searchHistoryFile.readAsString());
+      }
+      else {
+        await searchHistoryFile.create();
+        await searchHistoryFile.writeAsString(convert.jsonEncode({'searches' : []}));
+        searchHistory = {'searches' : []};
+      }
+    }
+    catch(e) {
+      searchHistory = {'searches' : []};
+    }
+    return searchHistory;
+  }
+
+  static Future<void> addSearchHistory(String keyword, String mode) async {
+    Directory externalDirectory = (await path.getExternalStorageDirectory());
+    Directory applicationDirectory = Directory(path.join(externalDirectory.path, '.harmonoid'));
+
+    File searchHistoryFile = File(path.join(applicationDirectory.path, 'search.json'));
+    
+    List<dynamic> searchHistory = (await SearchHistory.getSearchHistory())['searches'];
+    
+    if (searchHistory.length > 5 && searchHistory.isNotEmpty) {
+      searchHistory.removeAt(0);
+    }
+    searchHistory.add({
+      'keyword': keyword,
+      'mode': mode,
+    });
+    await searchHistoryFile.writeAsString(convert.jsonEncode({'searches' : searchHistory}));
+  }
+}
