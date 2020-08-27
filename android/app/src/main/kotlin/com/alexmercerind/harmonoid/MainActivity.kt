cc8681033ca878dfbb494c2ecdb66cb8e9bb1659
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-08-27 16:15:37 +0530

    🎉 Added splash screen

--- android/app/src/main/kotlin/com/alexmercerind/harmonoid/MainActivity.kt
+++ android/app/src/main/kotlin/com/alexmercerind/harmonoid/MainActivity.kt
@@ -2,5 +2,8 @@ package com.alexmercerind.harmonoid
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
