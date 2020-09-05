c94b5f3f3b73e92444c9de87516be896c96022a4
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-09-05 10:38:16 +0530

    👌 Improved local collection handling (No more loading bars)

--- lib/saved/savedalbumresults.dart
+++ lib/saved/savedalbumresults.dart
@@ -11,8 +11,11 @@ class AlbumTile extends StatelessWidget {
   final File albumArt;
   final Function refresh;
   final Map<String, dynamic> albumJson;
+  final List<TrackElement> albumTracks;
+  final List<Widget> albumLeadings;
+  final GlobalKey<SavedAlbumViewerState> globalKey;
 
-  AlbumTile({Key key, @required this.albumArt,  @required this.albumJson, @required this.refresh});
+  AlbumTile({Key key, @required this.albumLeadings, @required this.globalKey, @required this.albumArt,  @required this.albumJson, @required this.refresh, @required this.albumTracks});
 
   @override
   Widget build(BuildContext context) {
@@ -93,9 +96,12 @@ class AlbumTile extends StatelessWidget {
           ),
         ),
         openBuilder: (ctx, act) => SavedAlbumViewer(
+          key: this.globalKey,
           refresh: this.refresh,
           albumJson: this.albumJson,
           albumArt: this.albumArt,
+          albumLeadings: this.albumLeadings,
+          albumTracks: this.albumTracks,
         ),
       ),
     );
@@ -146,40 +152,179 @@ class SavedAlbumResultsState extends State<SavedAlbumResults> with SingleTickerP
 
   List<dynamic> _albums;
   List<File> _albumArts;
+  List<List<TrackElement>> _albumTracksList = new List<List<TrackElement>>();
+  List<List<Widget>> _albumLeadingsList = new List<List<Widget>>();
   List<Widget> _albumElements = new List<Widget>();
-  List<Widget> _listView = [
-    Center(
-      child: CircularProgressIndicator(),
-    )
-  ];
+  List<List<dynamic>> _tracksList = new List<List<dynamic>>();
+  List<Widget> _listView = new List<Widget>();
+  List<GlobalKey<SavedAlbumViewerState>> _albumsGlobalKey = new List<GlobalKey<SavedAlbumViewerState>>();
 
   Future<void> refresh() async {
     
-    this._albumElements.clear();
     this._albums = (await GetSavedMusic.albums())['albums'];
     this._albumArts = await GetSavedMusic.albumArts();
 
+    this._albumElements.clear();
+    this._albumTracksList.clear();
+    this._albumLeadingsList.clear();
+    this._albumElements.clear();
+    this._tracksList.clear();
+    this._listView.clear();
+    this._albumsGlobalKey.clear();
+
     if (this._albums.length == 0) {
       this.setState(() {
-
         this._listView = [
           NoResultsComponent(),
         ];
       });
     }
     else {
+      for (int index = 0; index < this._albums.length; index++) {
+        this._albumsGlobalKey.add(
+          new GlobalKey<SavedAlbumViewerState>(),
+        );
+        this._tracksList.add(
+          (await GetSavedMusic.tracks(this._albums[index]['album_id']))['tracks']
+        );
+      }
+
       this.setState(() {
         int elementsPerRow = MediaQuery.of(context).size.width ~/ 172.0;
         List<Widget> rowChildren = new List<Widget>();
         
         if (this._albums.length > 1) {
-          bool incompleteRow = (this._albums.length - 1) % elementsPerRow == 0 ? false : true;
-          for (int index = 0; index < this._albums.length - 1; index++) { 
+          bool incompleteRow = (this._albums.length) % elementsPerRow == 0 ? false : true;
+          for (int index = 0; index < this._albums.length; index++) {
+            List<TrackElement> albumTracks = [];
+            List<Widget> albumLeadings = [
+              Container(
+                  margin: EdgeInsets.only(left: 16, top: 24, bottom: 18),
+                  child: Text(
+                    Globals.STRING_LOCAL_ALBUM_VIEW_INFO_SUBHEADER,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 1,
+                  margin: EdgeInsets.only(left: 16, right: 16, top: 0, bottom: 0),
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.file(
+                          this._albumArts[index],
+                          height: 128,
+                          width: 128,
+                          fit: BoxFit.fill,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 18),
+                          width: MediaQuery.of(context).size.width - 16 - 16 - 128,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                this._albums[index]['album_name'].split('(')[0].trim().split('-')[0].trim(),
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black87,
+                                ),
+                                maxLines: 2,
+                                textAlign: TextAlign.start,
+                              ),
+                              Divider(
+                                color: Colors.white,
+                                height: 12,
+                                thickness: 12,
+                              ),
+                              Divider(
+                                color: Colors.white,
+                                height: 2,
+                                thickness: 2,
+                              ),
+                              Text(
+                                this._albums[index]['album_artists'].join(', '),
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.black54,
+                                ),
+                                maxLines: 2,
+                                textAlign: TextAlign.start,
+                              ),
+                              Divider(
+                                color: Colors.white,
+                                height: 2,
+                                thickness: 2,
+                              ),
+                              Text(
+                                '${this._albums[index]['year']}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.black54,
+                                ),
+                                maxLines: 1,
+                                textAlign: TextAlign.start,
+                              ),
+                              Divider(
+                                color: Colors.white,
+                                height: 2,
+                                thickness: 2,
+                              ),
+                              Text(
+                                '${this._albums[index]['album_length']}' + ' '+ Globals.STRING_TRACK.toLowerCase(),
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.black54,
+                                ),
+                                maxLines: 1,
+                                textAlign: TextAlign.start,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 16, top: 24, bottom: 18),
+                child: Text(
+                  Globals.STRING_LOCAL_ALBUM_VIEW_TRACKS_SUBHEADER,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+            ];
+            for (int albumIndex = 0; albumIndex < _tracksList[index].length; albumIndex++) {
+              albumTracks.add(
+                TrackElement(
+                  index: albumIndex,
+                  albumTracks: _tracksList[index],
+                  albumJson: this._albums[index],
+                  refresh: this.refresh,
+                  refreshTracks: () => this._albumsGlobalKey[index].currentState.refreshTracks(albumIndex),
+                  albumArt: this._albumArts[index],
+                ),
+              );
+            }
+            this._albumTracksList.add(albumTracks);
+            this._albumLeadingsList.add(albumLeadings);
             rowChildren.add(
               AlbumTile(
+                globalKey: this._albumsGlobalKey[index],
                 refresh: this.refresh,
                 albumArt: this._albumArts[index],
                 albumJson: this._albums[index],
+                albumTracks: this._albumTracksList[index],
+                albumLeadings: this._albumLeadingsList[index],
               ),
             );
             if (rowChildren.length == elementsPerRow) {
@@ -196,12 +341,15 @@ class SavedAlbumResultsState extends State<SavedAlbumResults> with SingleTickerP
 
           if (incompleteRow) {
             rowChildren = new List<Widget>();
-            for (int index = ((this._albums.length - 1) - ((this._albums.length - 1) % elementsPerRow)); index < this._albums.length - 1; index++) {
+            for (int index = ((this._albums.length) - ((this._albums.length) % elementsPerRow)); index < this._albums.length; index++) {
               rowChildren.add(
                 AlbumTile(
+                  globalKey: this._albumsGlobalKey[index],
                   refresh: this.refresh,
                   albumArt: this._albumArts[index],
                   albumJson: this._albums[index],
+                  albumTracks: _albumTracksList[index],
+                  albumLeadings: this._albumLeadingsList[index],
                 ),
               );
             }
@@ -308,9 +456,12 @@ class SavedAlbumResultsState extends State<SavedAlbumResults> with SingleTickerP
                 ),
               ),
               openBuilder: (ctx, act) => SavedAlbumViewer(
+                key: this._albumsGlobalKey[this._albumArts.length - 1],
                 refresh: this.refresh,
                 albumJson: this._albums[this._albumArts.length - 1],
                 albumArt: this._albumArts[this._albumArts.length - 1],
+                albumLeadings: this._albumLeadingsList[this._albumArts.length - 1],
+                albumTracks: this._albumTracksList[this._albumArts.length - 1],
               ),
             ),
           ),
