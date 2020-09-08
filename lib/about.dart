4cd75126757855499008b72409080ddc0a81d95e
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-09-08 17:25:12 +0530

    🐛 Fixed bug that caused download errors in some albums

--- lib/about.dart
+++ lib/about.dart
@@ -627,7 +627,7 @@ class AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Globals.globalTheme == 0 ? Colors.white.withOpacity(0.99) : Color(0xFF121212),
+      backgroundColor: Globals.globalTheme == 0 ? Colors.grey[50] : Color(0xFF121212),
       appBar: AppBar(
         brightness: Brightness.dark,
         backgroundColor: Globals.globalTheme == 0 ? Theme.of(context).primaryColor : Colors.white10,
