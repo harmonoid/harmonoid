03a7d2c0a7715eb3db598f75eb97ba27bf62b7dd
alexmercerind <saini123hitesh@gmail.com>
2020-12-29 18:51:11 +0530

    🔧 Updated structure & names of methods used during lifecycle

--- lib/scripts/states.dart
+++ lib/scripts/states.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:harmonoid/scripts/collection.dart';
+import 'package:harmonoid/scripts/configuration.dart';
+import 'package:harmonoid/constants/constantsupdater.dart';
+
 
 class States {
-  static dynamic musicCollectionCurrentTab;
-  static void Function(dynamic musicCollectionCurrentTab) musicCollectionRefresh;
-  static void Function() musicCollectionSearchRefresh;
-  static ThemeMode Function() switchTheme;
-  static void Function() showNowPlaying;
-  static Future<void> Function(Map<String, dynamic> track) setNowPlaying;
+  static dynamic musicCollectionCurrentTab = new Album();
+  static ThemeMode appTheme = ThemeMode.system;
+  static void Function(dynamic musicCollectionCurrentTab) refreshMusicCollection = (dynamic musicCollectionCurrentTab) {};
+  static void Function() refreshMusicSearch = () {};
+  static void Function(AppTheme appTheme) refreshAppTheme = (appTheme) {
+    switch(appTheme) {
+      case AppTheme.system: {
+        States.appTheme = ThemeMode.system;
+      }
+      break;
+      case AppTheme.light: {
+        States.appTheme = ThemeMode.light;
+      }
+      break;
+      case AppTheme.dark: {
+        States.appTheme = ThemeMode.dark;
+      }
+      break;
+      }
+  };
+  static void Function(LanguageRegion languageRegion) refreshLanguage = (LanguageRegion languageRegion) {
+    ConstantsUpdater.update(languageRegion.index);
+  };
 }
