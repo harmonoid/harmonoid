f67a9d85d627ed35d65b78f91fddcfea21e030ca
alexmercerind <saini123hitesh@gmail.com>
2021-01-24 16:57:49 +0000

    ✔ Cleared legacy app code

--- android/app/src/main/kotlin/com/alexmercerind/harmonoid/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.alexmercerind.harmonoid
-
-import io.flutter.embedding.android.FlutterActivity
-
-import android.os.Build
-import android.view.ViewTreeObserver
-import android.view.WindowManager
-class MainActivity: FlutterActivity() {
-}
