ad7c60c31434499739e20bdfd70fb107d6a927f6
alexmercerind <saini123hitesh@gmail.com>
2021-02-16 15:48:36 +0530

    👌 Added simple permission prompt on startup

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:animations/animations.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:harmonoid/language/language.dart';
-import 'package:harmonoid/scripts/discover.dart';
-import 'package:harmonoid/scripts/download.dart';
 import 'package:path_provider/path_provider.dart' as path;
 import 'package:audio_service/audio_service.dart';
 
@@ -16,6 +13,10 @@ import 'package:harmonoid/scripts/configuration.dart';
 import 'package:harmonoid/scripts/playback.dart';
 import 'package:harmonoid/screens/nowplaying.dart';
 import 'package:harmonoid/screens/exception.dart';
+import 'package:harmonoid/language/language.dart';
+import 'package:harmonoid/scripts/discover.dart';
+import 'package:harmonoid/scripts/download.dart';
+import 'package:harmonoid/scripts/methods.dart';
 import 'package:harmonoid/scripts/vars.dart';
 
 
@@ -26,6 +27,7 @@ void main() async {
     await Configuration.init(
       cacheDirectory: await path.getExternalStorageDirectory(),
     );
+    await Methods.askStoragePermission();
     await Collection.init(
       collectionDirectory: Directory(MUSIC_DIRECTORY),
       cacheDirectory: await path.getExternalStorageDirectory(),
@@ -77,219 +79,13 @@ class HarmonoidState extends State<Harmonoid> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'harmonoid',
-      theme: new ThemeData(
-        splashFactory: InkRipple.splashFactory,
-        highlightColor: Colors.transparent,
-        brightness: Brightness.light,
-        primaryColorLight: Colors.deepPurpleAccent,
-        primaryColor: Colors.deepPurpleAccent[400],
-        primaryColorDark: Colors.deepPurpleAccent[700],
-        scaffoldBackgroundColor: Colors.grey[100],
-        cursorColor: Colors.deepPurpleAccent[700],
-        accentColor: Colors.deepPurpleAccent[400],
-        textSelectionHandleColor: Colors.deepPurpleAccent[400],
-        toggleableActiveColor: Colors.deepPurpleAccent[400],
-        cardColor: Colors.white,
-        backgroundColor: Colors.deepPurpleAccent[100],
-        dividerColor: Colors.black12,
-        disabledColor: Colors.black38,
-        tabBarTheme: TabBarTheme(
-          labelColor: Colors.deepPurpleAccent[700],
-          unselectedLabelColor: Colors.black54,
-        ),
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          brightness: Brightness.light,
-          elevation: 4.0,
-          iconTheme: IconThemeData(
-            color: Colors.black54,
-            size: 24,
-          ),
-          actionsIconTheme: IconThemeData(
-            color: Colors.black54,
-            size: 24,
-          ),
-        ),
-        iconTheme: IconThemeData(
-          color: Colors.black54,
-          size: 24,
-        ),
-        bottomNavigationBarTheme: new BottomNavigationBarThemeData(
-          backgroundColor: Colors.deepPurpleAccent[700],
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white54,
-        ),
-        primaryTextTheme: new TextTheme(
-          headline1: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black87,
-            fontSize: 18,
-          ),
-          headline2: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black87,
-            fontSize: 16,
-          ),
-          headline3: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black54,
-            fontSize: 16,
-          ),
-          headline4: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black87,
-            fontSize: 14,
-          ),
-          headline5: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black54,
-            fontSize: 14,
-          ),
-          headline6: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black87,
-            fontSize: 18,
-          ),
-        ),
-        textTheme: new TextTheme(
-          headline1: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black87,
-            fontSize: 18,
-          ),
-          headline2: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black87,
-            fontSize: 16,
-          ),
-          headline3: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black54,
-            fontSize: 16,
-          ),
-          headline4: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black87,
-            fontSize: 14,
-          ),
-          headline5: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black54,
-            fontSize: 14,
-          ),
-          headline6: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.black87,
-            fontSize: 18,
-          ),
-        ),
+      theme: Methods.getThemeData(
+        color: Colors.deepPurpleAccent[700],
+        themeMode: ThemeMode.light,
       ),
-      darkTheme: new ThemeData(
-        splashFactory: InkRipple.splashFactory,
-        highlightColor: Colors.transparent,
-        brightness: Brightness.dark,
-        primaryColorLight: Colors.deepPurpleAccent[100],
-        primaryColor: Colors.deepPurpleAccent[100],
-        primaryColorDark: Colors.deepPurpleAccent[100],
-        scaffoldBackgroundColor: Color(0xFF121212),
-        cursorColor: Colors.deepPurpleAccent[100],
-        accentColor: Colors.deepPurpleAccent[100],
-        textSelectionHandleColor: Colors.deepPurpleAccent[100],
-        toggleableActiveColor: Colors.deepPurpleAccent[100],
-        cardColor: Colors.white.withOpacity(0.14),
-        backgroundColor: Colors.deepPurple[100],
-        dividerColor: Colors.white12,
-        disabledColor: Colors.white38,
-        tabBarTheme: TabBarTheme(
-          labelColor: Colors.deepPurpleAccent[100],
-          unselectedLabelColor: Colors.white.withOpacity(0.54),
-        ),
-        appBarTheme: AppBarTheme(
-          color: Color.fromRGBO(42, 42, 42, 1),
-          brightness: Brightness.dark,
-          elevation: 4.0,
-          iconTheme: IconThemeData(
-            color: Colors.black54,
-            size: 24,
-          ),
-          actionsIconTheme: IconThemeData(
-            color: Colors.black54,
-            size: 24,
-          ),
-        ),
-        iconTheme: IconThemeData(
-          color: Colors.white.withOpacity(0.54),
-          size: 24,
-        ),
-        bottomNavigationBarTheme: new BottomNavigationBarThemeData(
-          backgroundColor: Color.fromRGBO(42, 42, 42, 1),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white54,
-        ),
-        primaryTextTheme: new TextTheme(
-          headline1: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.87),
-            fontSize: 18,
-          ),
-          headline2: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.87),
-            fontSize: 16,
-          ),
-          headline3: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.54),
-            fontSize: 16,
-          ),
-          headline4: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.87),
-            fontSize: 14,
-          ),
-          headline5: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.54),
-            fontSize: 14,
-          ),
-          headline6: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.87),
-            fontSize: 18,
-          ),
-        ),
-        textTheme: new TextTheme(
-          headline1: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.87),
-            fontSize: 18,
-          ),
-          headline2: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.87),
-            fontSize: 16,
-          ),
-          headline3: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.54),
-            fontSize: 16,
-          ),
-          headline4: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.87),
-            fontSize: 14,
-          ),
-          headline5: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.54),
-            fontSize: 14,
-          ),
-          headline6: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.white.withOpacity(0.87),
-            fontSize: 18,
-          ),
-        ),
+      darkTheme: Methods.getThemeData(
+        color: Colors.deepPurpleAccent[100],
+        themeMode: ThemeMode.dark,
       ),
       themeMode: configuration.themeMode,
       home: Home(),
@@ -297,8 +93,8 @@ class HarmonoidState extends State<Harmonoid> {
         PageRoute route;
         if (routeSettings.name == 'nowPlaying') {
           route = new PageRouteBuilder(
-            transitionDuration: Duration(milliseconds: 400),
-            reverseTransitionDuration: Duration(milliseconds: 400),
+            transitionDuration: Duration(milliseconds: 700),
+            reverseTransitionDuration: Duration(milliseconds: 700),
             transitionsBuilder: (context, animation, secondaryAnimation, child) => FadeThroughTransition(
               animation: animation,
               secondaryAnimation: secondaryAnimation,
--- lib/scripts/methods.dart
+++ lib/scripts/methods.dart
@@ -1,5 +1,8 @@
 
 import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'package:harmonoid/language/constants.dart';
 import 'package:harmonoid/scripts/collection.dart';
@@ -41,5 +44,127 @@ abstract class Methods {
       return Constants.STRING_PLAYLIST;
     else
       return null;
+  }
+
+  static Future<void> askStoragePermission() async {
+    if (await Permission.storage.isUndetermined || await Permission.storage.isDenied) {
+      PermissionStatus storagePermissionState = await Permission.storage.request();
+      if (!storagePermissionState.isGranted) {
+        SystemNavigator.pop(
+          animated: true,
+        );
+      }
+    }
+  }
+
+  static ThemeData getThemeData({Color color, ThemeMode themeMode}) {
+    bool isLightTheme = themeMode == ThemeMode.light;
+    return new ThemeData(
+      splashFactory: InkRipple.splashFactory,
+      highlightColor: Colors.transparent,
+      brightness: isLightTheme ? Brightness.light : Brightness.dark,
+      primaryColorLight: color,
+      primaryColor: color,
+      primaryColorDark: color,
+      scaffoldBackgroundColor: isLightTheme ? Colors.grey[100] : Color(0xFF121212),
+      cursorColor: color,
+      accentColor: color,
+      textSelectionHandleColor: color,
+      toggleableActiveColor: color,
+      cardColor: isLightTheme ? Colors.white : Colors.white.withOpacity(0.08),
+      backgroundColor: color.withOpacity(0.24),
+      dividerColor: isLightTheme ? Colors.black12 : Colors.white24,
+      disabledColor: isLightTheme ? Colors.black38 : Colors.white38,
+      tabBarTheme: TabBarTheme(
+        labelColor: color,
+        unselectedLabelColor: isLightTheme ? Colors.black54 : Colors.white.withOpacity(0.67),
+      ),
+      appBarTheme: AppBarTheme(
+        color: isLightTheme ? Colors.white : Color.fromRGBO(42, 42, 42, 1),
+        brightness: isLightTheme ? Brightness.light : Brightness.dark,
+        elevation: 4.0,
+        iconTheme: IconThemeData(
+          color: isLightTheme ? Colors.black54 : Colors.white.withOpacity(1.0),
+          size: 24,
+        ),
+        actionsIconTheme: IconThemeData(
+          color: isLightTheme ? Colors.black54 : Colors.white.withOpacity(1.0),
+          size: 24,
+        ),
+      ),
+      iconTheme: IconThemeData(
+        color: isLightTheme ? Colors.black54 : Colors.white.withOpacity(1.0),
+        size: 24,
+      ),
+      bottomNavigationBarTheme: new BottomNavigationBarThemeData(
+        backgroundColor: isLightTheme ? color : Color.fromRGBO(42, 42, 42, 1),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white54,
+      ),
+      primaryTextTheme: new TextTheme(
+        headline1: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black87 : Colors.white.withOpacity(0.87),
+          fontSize: 18,
+        ),
+        headline2: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black87 : Colors.white.withOpacity(0.87),
+          fontSize: 16,
+        ),
+        headline3: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black54 : Colors.white.withOpacity(0.54),
+          fontSize: 16,
+        ),
+        headline4: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black87 : Colors.white.withOpacity(0.87),
+          fontSize: 14,
+        ),
+        headline5: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black54 : Colors.white.withOpacity(0.54),
+          fontSize: 14,
+        ),
+        headline6: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black87 : Colors.white.withOpacity(1.0),
+          fontSize: 18,
+        ),
+      ),
+      textTheme: new TextTheme(
+        headline1: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black87 : Colors.white.withOpacity(0.87),
+          fontSize: 18,
+        ),
+        headline2: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black87 : Colors.white.withOpacity(0.87),
+          fontSize: 16,
+        ),
+        headline3: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black54 : Colors.white.withOpacity(0.54),
+          fontSize: 16,
+        ),
+        headline4: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black87 : Colors.white.withOpacity(0.87),
+          fontSize: 14,
+        ),
+        headline5: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black54 : Colors.white.withOpacity(0.54),
+          fontSize: 14,
+        ),
+        headline6: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: isLightTheme ? Colors.black87 : Colors.white.withOpacity(1.0),
+          fontSize: 18,
+        ),
+      ),
+    );
   }
 }
