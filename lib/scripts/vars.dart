b1cb8baa56ddd1ee45ab33505bea51e17011e842
alexmercerind <saini123hitesh@gmail.com>
2021-02-18 20:26:44 +0530

    👌 Defined accent colors

--- lib/scripts/vars.dart
+++ lib/scripts/vars.dart
@@ -1,2 +1,16 @@
+import 'package:flutter/material.dart';
 
-const String MUSIC_DIRECTORY = '/storage/emulated/0/Music';
+
+String MUSIC_DIRECTORY = '/storage/emulated/0/Music';
+List<List<Color>> ACCENT_COLORS = [
+  [Color(0xFF651FFF), Color(0xFFB388FF)],
+  [Color(0xFF4285F4), Color(0xFF82B1FF)],
+  [Color(0xFFDB4437), Color(0xFFFF8A80)],
+  [Color(0xFFF4B400), Color(0xFFFFE57F)],
+  [Color(0xFF0F9D58), Color(0xFF0F9D58)],
+  [Color(0xFFFF3D00), Color(0xFFFF9E80)],
+  [Color(0xFF1DB954), Color(0xFF1DB954)],
+  [Color(0xFF5B51D8), Color(0xFFD1C4E9)],
+  [Color(0xFFF50057), Color(0xFFFF80AB)],
+  [Color(0xFF424242), Color(0xFFE0E0E0)],
+];
