c48ac3d062619c8795fd98420ec6ca2ba8614889
alexmercerind <saini123hitesh@gmail.com>
2021-02-21 13:32:49 +0530

    🎉 Implemented track search

--- lib/screens/discover/discoversearch.dart
+++ lib/screens/discover/discoversearch.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:harmonoid/screens/discover/discovertrack.dart';
 
 import 'package:harmonoid/scripts/collection.dart';
 import 'package:harmonoid/scripts/discover.dart';
@@ -31,6 +32,78 @@ class DiscoverSearchState extends State<DiscoverSearch> {
       this._tileHeight = this._tileWidth * 242 / 156;
     }
     this._init = false;
+  }
+
+  Widget get result {
+    if (widget.mode is Album)
+      return FadeFutureBuilder(
+        future: () async => await discover.search(widget.keyword, widget.mode),
+        initialWidgetBuilder: (BuildContext context) => FakeLinearProgressIndicator(
+          label: Constants.STRING_SEARCH_RESULT_LOADER_LABEL,
+          duration: Duration(seconds: 10),
+          width: 148.0,
+          margin: EdgeInsets.only(top: 196.0),
+        ),
+        finalWidgetBuilder: (BuildContext context, Object data) => Column(
+          children: tileGridListWidgets(
+            context: context,
+            tileHeight: this._tileHeight,
+            tileWidth: this._tileWidth,
+            elementsPerRow: this._elementsPerRow,
+            leadingSubHeader: Constants.STRING_SEARCH_RESULT_TOP_SUBHEADER_ALBUM,
+            subHeader: Constants.STRING_SEARCH_RESULT_OTHER_SUBHEADER_ALBUM,
+            leadingWidget: LeadingDiscoverAlbumTile(
+              height: this._tileWidth,
+              album: (data as List<dynamic>).first,
+            ),
+            widgetCount: (data as List<dynamic>).length,
+            builder: (BuildContext context, int index) => DiscoverAlbumTile(
+              album: (data as List<dynamic>)[index],
+              height: this._tileHeight,
+              width: this._tileWidth,
+            )
+          ),
+        ),
+        errorWidgetBuilder: (_, Object exception) => ExceptionWidget(
+          margin: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+          height: 156.0,
+          assetImage: 'assets/images/exception.jpg',
+          title: Constants.STRING_NO_INTERNET_TITLE,
+          subtitle: Constants.STRING_NO_INTERNET_SUBTITLE,
+        ),
+        transitionDuration: Duration(milliseconds: 400),
+      );
+    else if (widget.mode is Track)
+      return FadeFutureBuilder(
+        future: () async => await discover.search(widget.keyword, widget.mode),
+        initialWidgetBuilder: (BuildContext context) => FakeLinearProgressIndicator(
+          label: Constants.STRING_SEARCH_RESULT_LOADER_LABEL,
+          duration: Duration(seconds: 10),
+          width: 148.0,
+          margin: EdgeInsets.only(top: 196.0),
+        ),
+        finalWidgetBuilder: (BuildContext context, Object data) => Column(
+          children: <Widget>[
+            SubHeader(Constants.STRING_SEARCH_RESULT_TOP_SUBHEADER_TRACK),
+            LeadingDiscoverTrackTile(
+              track: (data as List<dynamic>)[0],
+            ),
+            SubHeader(Constants.STRING_SEARCH_RESULT_OTHER_SUBHEADER_TRACK)
+          ] + (data as List<dynamic>).map(
+            (dynamic track) => DiscoverTrackTile(
+              track: track,
+            ),
+          ).toList(),
+        ),
+        errorWidgetBuilder: (_, Object exception) => ExceptionWidget(
+          margin: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+          height: 156.0,
+          assetImage: 'assets/images/exception.jpg',
+          title: Constants.STRING_NO_INTERNET_TITLE,
+          subtitle: Constants.STRING_NO_INTERNET_SUBTITLE,
+        ),
+        transitionDuration: Duration(milliseconds: 400),
+      );
   }
 
   @override
@@ -67,43 +140,7 @@ class DiscoverSearchState extends State<DiscoverSearch> {
           SliverList(
             delegate: SliverChildListDelegate(
               <Widget>[
-                FadeFutureBuilder(
-                  future: () async => await discover.search(widget.keyword, widget.mode),
-                  initialWidgetBuilder: (BuildContext context) => FakeLinearProgressIndicator(
-                    label: Constants.STRING_SEARCH_RESULT_LOADER_LABEL,
-                    duration: Duration(seconds: 10),
-                    width: 156.0,
-                    margin: EdgeInsets.only(top: 196.0),
-                  ),
-                  finalWidgetBuilder: (BuildContext context, Object data) => Column(
-                    children: tileGridListWidgets(
-                      context: context,
-                      tileHeight: this._tileHeight,
-                      tileWidth: this._tileWidth,
-                      elementsPerRow: this._elementsPerRow,
-                      leadingSubHeader: Constants.STRING_SEARCH_RESULT_TOP_SUBHEADER_ALBUM,
-                      subHeader: Constants.STRING_SEARCH_RESULT_OTHER_SUBHEADER_ALBUM,
-                      leadingWidget: LeadingDiscoverAlbumTile(
-                        height: this._tileWidth,
-                        album: (data as List<dynamic>).first,
-                      ),
-                      widgetCount: (data as List<dynamic>).length,
-                      builder: (BuildContext context, int index) => DiscoverAlbumTile(
-                        album: (data as List<dynamic>)[index],
-                        height: this._tileHeight,
-                        width: this._tileWidth,
-                      )
-                    ),
-                  ),
-                  errorWidgetBuilder: (_, Object exception) => ExceptionWidget(
-                    margin: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
-                    height: 156.0,
-                    assetImage: 'assets/images/exception.jpg',
-                    title: Constants.STRING_NO_INTERNET_TITLE,
-                    subtitle: Constants.STRING_NO_INTERNET_SUBTITLE,
-                  ),
-                  transitionDuration: Duration(milliseconds: 400),
-                ),
+                this.result,
               ],
             ),
           ),
