05ba8cc34ebcacece04b52c0b3fd526abb5b1c45
alexmercerind <saini123hitesh@gmail.com>
2021-03-07 02:25:00 +0530

    🔧 Other classes extracted from legacy code.

---
+++ lib/core/playback.dart
@@ -0,0 +1,44 @@
+import 'package:assets_audio_player/assets_audio_player.dart' as AudioPlayer;
+
+import 'package:harmonoid/core/collection.dart';
+
+
+final AudioPlayer.AssetsAudioPlayer audioPlayer = new AudioPlayer.AssetsAudioPlayer.withId('harmonoid');
+
+
+class Playback {
+  static Future<void> play({int index, List<Track> tracks}) async {
+    List<AudioPlayer.Audio> audios = <AudioPlayer.Audio>[];
+    tracks.forEach((Track track) {
+      audios.add(
+        new AudioPlayer.Audio.file(
+          track.filePath,
+          metas: new AudioPlayer.Metas(
+            id: track.trackId,
+            image: new AudioPlayer.MetasImage.file(
+              track.albumArt.path,
+            ),
+            title: track.trackName,
+            album: track.albumName,
+            artist: track.trackArtistNames.join(', '),
+            extra: track.toMap(),
+          )
+        ),
+      );
+    });
+    audioPlayer.open(
+      new AudioPlayer.Playlist(
+        audios: audios,
+        startIndex: index,
+      ),
+      showNotification: true,
+      notificationSettings: new AudioPlayer.NotificationSettings(
+        playPauseEnabled: true,
+        nextEnabled: true,
+        prevEnabled: true,
+        seekBarEnabled: true,
+        stopEnabled: false,   
+      ),
+    );
+  }
+}
