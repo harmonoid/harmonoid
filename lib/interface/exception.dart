eab71a344f8ec66fe8ae2e35aabaf701301c9102
alexmercerind <saini123hitesh@gmail.com>
2021-03-07 02:36:47 +0530

    👌 Added Collection ChangeNotifierProvider to improve collection handling

---
+++ lib/interface/exception.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
+
+import 'package:harmonoid/interface/settings/settings.dart';
+
+
+class ExceptionMaterialApp extends StatelessWidget {
+  final dynamic exception;
+  ExceptionMaterialApp({Key key, @required this.exception}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        splashFactory: InkRipple.splashFactory,
+        highlightColor: Colors.transparent,
+        brightness: Brightness.light,
+        primaryColorLight: Colors.deepPurpleAccent,
+        primaryColor: Colors.deepPurpleAccent[400],
+        primaryColorDark: Colors.deepPurpleAccent[700],
+        scaffoldBackgroundColor: Colors.grey[100],
+        accentColor: Colors.deepPurpleAccent[400],
+        toggleableActiveColor: Colors.deepPurpleAccent[400],
+        cardColor: Colors.white,
+        backgroundColor: Colors.deepPurpleAccent[100],
+        dividerColor: Colors.black12,
+        disabledColor: Colors.black38,
+        tabBarTheme: TabBarTheme(
+          labelColor: Colors.deepPurpleAccent[700],
+          unselectedLabelColor: Colors.black54,
+        ),
+        appBarTheme: AppBarTheme(
+          color: Colors.deepPurpleAccent[700],
+          brightness: Brightness.dark,
+          elevation: 4.0,
+          iconTheme: IconThemeData(
+            color: Colors.white,
+            size: 24,
+          ),
+          actionsIconTheme: IconThemeData(
+            color: Colors.white,
+            size: 24,
+          ),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.black54,
+          size: 24,
+        ),
+        bottomNavigationBarTheme: new BottomNavigationBarThemeData(
+          backgroundColor: Colors.deepPurpleAccent[700],
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white54,
+        ),
+        primaryTextTheme: new TextTheme(
+          headline1: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black87,
+            fontSize: 18,
+          ),
+          headline2: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black87,
+            fontSize: 16,
+          ),
+          headline3: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black54,
+            fontSize: 16,
+          ),
+          headline4: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black87,
+            fontSize: 14,
+          ),
+          headline5: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black54,
+            fontSize: 14,
+          ),
+          headline6: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+        textTheme: new TextTheme(
+          headline1: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black87,
+            fontSize: 18,
+          ),
+          headline2: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black87,
+            fontSize: 16,
+          ),
+          headline3: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black54,
+            fontSize: 16,
+          ),
+          headline4: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black87,
+            fontSize: 14,
+          ),
+          headline5: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black54,
+            fontSize: 14,
+          ),
+          headline6: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.light,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.error),
+          title: Text('Harmonoid'),
+        ),
+        body: ListView(
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          children: [
+            SettingsTile(
+              title: 'Exception Occured',
+              subtitle: 'Something wrong is experienced',
+              child: Column(
+                children: [
+                  Text('$exception'),
+                  Divider(
+                    color: Colors.transparent,
+                    height: 16.0,
+                  ),
+                  Text(
+                    'You may try to clear app cache or report this on the project repository.',
+                    style: TextStyle(
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+              actions: [
+                MaterialButton(
+                  onPressed: () => UrlLauncher.launch('https://github.com/alexmercerind/harmonoid/issues'),
+                  child: Text(
+                    'REPORT ISSUE',
+                    style: TextStyle(
+                      color: Colors.deepPurpleAccent[700]
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: SystemNavigator.pop,
+                  child: Text(
+                    'EXIT APP',
+                    style: TextStyle(
+                      color: Colors.deepPurpleAccent[700]
+                    ),
+                  ),
+                ),
+              ],
+              margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+            ),
+            Card(
+              elevation: 2.0,
+              margin: EdgeInsets.all(8.0),
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.phone_android),
+                    title: Text(
+                      'Your music will stay safe',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/interface/home.dart
@@ -0,0 +1,183 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:animations/animations.dart';
+import 'package:flutter/services.dart';
+
+import 'package:harmonoid/core/collection.dart';
+import 'package:harmonoid/core/fileintent.dart';
+import 'package:harmonoid/interface/collection/collectionmusic.dart';
+import 'package:harmonoid/interface/collection/collectionsearch.dart';
+import 'package:harmonoid/interface/nowplaying.dart';
+import 'package:harmonoid/interface/settings/settings.dart';
+import 'package:harmonoid/constants/language.dart';
+
+
+enum Screen {
+  nowPlaying,
+  collection,
+  settings,
+}
+
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key : key);
+  HomeState createState() => HomeState();
+}
+
+
+class HomeState extends State<Home> with TickerProviderStateMixin, WidgetsBindingObserver {
+  int index = fileIntent.startScreen.index;
+  List<GlobalKey<NavigatorState>> navigatorKeys = <GlobalKey<NavigatorState>>[
+    new GlobalKey<NavigatorState>(),
+    new GlobalKey<NavigatorState>(),
+    new GlobalKey<NavigatorState>(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    if (fileIntent.startScreen == Screen.nowPlaying) fileIntent.play();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Future<bool> didPopRoute() async {
+    if (this.navigatorKeys[this.index].currentState.canPop()) {
+      this.navigatorKeys[this.index].currentState.pop();
+    }
+    else {
+      showDialog(
+        context: context,
+        builder: (subContext) => AlertDialog(
+          title: Text(
+            language.STRING_EXIT_TITLE,
+            style: Theme.of(subContext).textTheme.headline1,
+          ),
+          content: Text(
+            language.STRING_EXIT_SUBTITLE,
+            style: Theme.of(subContext).textTheme.headline5,
+          ),
+          actions: [
+            MaterialButton(
+              textColor: Theme.of(context).primaryColor,
+              onPressed: SystemNavigator.pop,
+              child: Text(language.STRING_YES),
+            ),
+            MaterialButton(
+              textColor: Theme.of(context).primaryColor,
+              onPressed: Navigator.of(subContext).pop,
+              child: Text(language.STRING_NO),
+            ),
+          ],
+        ),
+      );
+    }
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> screens = <Widget>[
+      Navigator(
+        key: this.navigatorKeys[0],
+        initialRoute: 'nowPlaying',
+        onGenerateRoute: (RouteSettings routeSettings) {
+          Route route;
+          if (routeSettings.name == 'nowPlaying') {
+            route = MaterialPageRoute(
+              builder: (BuildContext context) => NowPlaying(),
+            );
+          }
+          return route;
+        },
+      ),
+      Navigator(
+        key: this.navigatorKeys[1],
+        initialRoute: 'collectionMusic',
+        onGenerateRoute: (RouteSettings routeSettings) {
+          Route<dynamic> route;
+          if (routeSettings.name == 'collectionMusic') {
+            route = new MaterialPageRoute(builder: (BuildContext context) => CollectionMusic());
+          }
+          if (routeSettings.name == 'collectionSearch') {
+            route = new PageRouteBuilder(
+              transitionDuration: Duration(milliseconds: 400),
+              reverseTransitionDuration: Duration(milliseconds: 400),
+              transitionsBuilder: (context, animation, secondaryAnimation, child) => FadeThroughTransition(
+                animation: animation,
+                secondaryAnimation: secondaryAnimation,
+                child: child,
+              ),
+              pageBuilder: (context, animation, secondaryAnimation) => Consumer<Collection>(
+                builder: (context, collection, _) => CollectionSearch(),
+              ),
+            );
+          }
+          return route;
+        },
+      ),
+      Navigator(
+        key: this.navigatorKeys[2],
+        initialRoute: 'settings',
+        onGenerateRoute: (RouteSettings routeSettings) {
+          Route route;
+          if (routeSettings.name == 'settings') {
+            route = MaterialPageRoute(
+              builder: (BuildContext context) => Settings(),
+            );
+          }
+          return route;
+        },
+      ),
+    ];
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<Collection>(create: (context) => Collection.get()),
+        ChangeNotifierProvider<Language>(create: (context) => Language.get()),
+      ],
+      builder: (context, _) => Consumer<Language>(
+        builder: (context, _, __) => Scaffold(
+          body: PageTransitionSwitcher(
+            child: screens[this.index],
+            duration: Duration(milliseconds: 400),
+            transitionBuilder: (child, animation, secondaryAnimation) => FadeThroughTransition(
+              animation: animation,
+              secondaryAnimation: secondaryAnimation,
+              fillColor: Theme.of(context).scaffoldBackgroundColor,
+              child: child,
+            ),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.shifting,
+            currentIndex: this.index,
+            onTap: (int index) => this.setState(() => this.index = index),
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.play_arrow),
+                label: language.STRING_NOW_PLAYING,
+                backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.library_music),
+                label: language.STRING_COLLECTION,
+                backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: language.STRING_SETTING,
+                backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
