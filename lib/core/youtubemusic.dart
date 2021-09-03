0eea526ef0df8053084ee77602b28f3a575c4787
alexmercerind <saini123hitesh@gmail.com>
2021-09-03 13:05:29 +0530

    🩹 Improve YouTube playback speed

--- lib/core/youtubemusic.dart
+++ lib/core/youtubemusic.dart
@@ -1,6 +1,6 @@
+// ignore_for_file: empty_catches
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 import 'package:harmonoid/core/collection.dart';
 
@@ -36,8 +36,6 @@ const Map<String, dynamic> REQUEST_PAYLOAD = {
 };
 
 abstract class Client {
-  static YoutubeExplode external = YoutubeExplode();
-
   static Future<http.Response> request(
       String path, Map<String, String> properties) async {
     var response = await http.post(
@@ -151,37 +149,29 @@ abstract class YTM {
 extension TrackExtension on Track {
   Future<void> attachAudioStream() async {
     if (filePath != null) return;
-    var video = await Client.external.videos.get(trackId);
-    var manifest =
-        await Client.external.videos.streamsClient.getManifest(video.id);
-    var description = video.description.split('\n\n');
-    var year;
-    try {
-      if (description.length >= 5) {
-        year = description[4].replaceAll('Released on: ', '').split('-').first;
-      }
-    } catch (e) {}
-    try {
-      trackName = trackName ?? video.title;
-    } catch (e) {}
-    try {
-      trackDuration = trackDuration ?? video.duration!.inMilliseconds;
-    } catch (e) {}
-    try {
-      trackArtistNames =
-          trackArtistNames ?? description[1].split(' · ').sublist(1);
-    } catch (e) {}
-    try {
-      albumName = albumName ?? description[2].trim();
-    } catch (e) {}
-    try {
-      albumArtistName =
-          albumArtistName ?? description[1].split(' · ').sublist(1).first;
-    } catch (e) {}
-    try {
-      year = year ?? int.tryParse(year);
-    } catch (e) {}
-    filePath = filePath ?? manifest.audio.withHighestBitrate().url.toString();
+    var video = await Client.request(
+      'player',
+      {
+        'videoId': trackId!,
+      },
+    );
+    var preferred;
+    var fallback;
+    var body =
+        convert.jsonDecode(video.body)['streamingData']['adaptiveFormats'];
+    for (var format in body) {
+      if (format['itag'] == 251) preferred = format['signatureCipher'];
+      if (format['itag'] == 18) fallback = format['signatureCipher'];
+    }
+    // Apparently YouTube Explode (Dart?) is very slow & unnecessarily way too much object oriented to
+    // sub-class or expose certain utils. Its very inefficient how it deciphers the URL,
+    // so decided to write own decipher backend using internal PyTube utils.
+    //
+    //The source code can be found in a comment below.
+    //
+    var stream = await http.get(Uri.parse(
+        'https://yt-music-headless.vercel.app/decipher?${preferred ?? fallback}'));
+    filePath = stream.body;
   }
 
   Future<List<Track>> get recommendations async {
@@ -238,3 +228,54 @@ extension TrackExtension on Track {
     return result.cast();
   }
 }
+
+// import asyncio
+// import aiohttp
+// from pytube import YouTube
+// from pytube.extract import apply_signature
+// from pytube import YouTube, extract, __js_url__
+// from fastapi import FastAPI, Response
+// class Extractor(YouTube):
+//     def __init__(self):
+//         self._js_url: str = None
+//         self._js: str = None
+//         self.streams_data = []
+//         self.stream_url = {}
+//     async def get_javascript(self) -> None:
+//         async with aiohttp.ClientSession() as session:
+//             async with session.get('https://www.youtube.com/watch') as response:
+//                 watch_html = await response.text()
+//                 loop = asyncio.get_event_loop()
+//                 self._js_url = await loop.run_in_executor(None, extract.js_url, watch_html)
+//                 if __js_url__ != self._js_url:
+//                     async with aiohttp.ClientSession() as session:
+//                         async with session.get(self._js_url) as response:
+//                             self._js = await response.text()
+//     async def get_stream_url(self, s, url):
+//         self.streams_data = [{
+//             'itag': 0,
+//             's': s,
+//             'url': url,
+//         }]
+//         self._player_response = {
+//             'player_response': {'streamingData': self.streams_data}}
+//         await self._decipher()
+//         return self.stream_url
+//     async def _decipher(self, retry: bool = False) -> None:
+//         if not self._js or retry:
+//             await self.get_javascript()
+//         loop = asyncio.get_event_loop()
+//         await loop.run_in_executor(None, apply_signature, self.streams_data, self._player_response, self._js)
+//         self.stream_url = self.streams_data[0]['url']
+// app = FastAPI()
+// extractor = Extractor()
+// @app.on_event('startup')
+// async def startup():
+//     try:
+//         await extractor.get_javascript()
+//     except:
+//         pass
+// @app.get('/decipher')
+// async def decipher(s: str, url: str):
+//     url = await extractor.get_stream_url(s, url)
+//     return Response(url, 200)
