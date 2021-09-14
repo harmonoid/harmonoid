4552449f78b6eb95bced600cc8f6f4643e258ec5
alexmercerind <saini123hitesh@gmail.com>
2021-09-14 15:04:27 +0530

    🏗 Linux: Change default window dimensions

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -22,7 +22,7 @@ static void my_application_activate(GApplication *application)
   GtkWindow *window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
   gtk_window_set_title(window, "Harmonoid");
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 1366, 768);
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
   FlView *view = fl_view_new(project);
