96ebecd670d686d5583bdefbaa221a332bd57f33
alexmercerind <saini123hitesh@gmail.com>
2021-09-22 16:06:01 +0530

    🔖 Bump version to v0.1.7

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:harmonoid/interface/exception.dart';
 import 'package:harmonoid/constants/language.dart';
 
 const String TITLE = 'Harmonoid';
-const String VERSION = '0.1.6';
+const String VERSION = '0.1.7';
 const String AUTHOR = 'Hitesh Kumar Saini <saini123hitesh@gmail.com>';
 const String LICENSE = 'GPL-3.0';
 
