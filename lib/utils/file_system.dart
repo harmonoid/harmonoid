f392b310165c4a796df6e1b14d11e7514c4e962c
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-02-28 16:35:40 +0530

    ⚡ Add custom file system related implementations - Add Directory.list_. - Add FileSystemEntity.delete_. - Add FileSystemEntity.exists_. - Add FileSystemEntity.existsSync_. - Thanks to @Sombian for report.

---
+++ lib/utils/file_system.dart
@@ -0,0 +1,82 @@
+/// This file is a part of Harmonoid (https://github.com/harmonoid/harmonoid).
+///
+/// Copyright © 2020-2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+///
+/// Use of this source code is governed by the End-User License Agreement for Harmonoid that can be found in the EULA.txt file.
+///
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+
+extension DirectoryExtension on Directory {
+  /// Recursively lists all the present [File]s inside the [Directory].
+  ///
+  /// * Safely handles long file-paths on Windows (https://github.com/dart-lang/sdk/issues/27825).
+  /// * Does not terminate on errors e.g. an encounter of `Access Is Denied`.
+  /// * Does not follow links.
+  /// * Returns only [List] of [File]s.
+  ///
+  Future<List<File>> list_() async {
+    final prefix = Platform.isWindows ? r'\\?\' : '';
+    final completer = Completer();
+    final files = <File>[];
+    Directory(prefix + path)
+        .list(
+      recursive: true,
+      followLinks: false,
+    )
+        .listen(
+      (event) {
+        if (event is File) {
+          files.add(File(event.path.substring(4)));
+        }
+      },
+      onError: (error) {
+        // For debugging. In case any future error is reported.
+        debugPrint('Directory.list_: ${error}');
+      },
+      onDone: completer.complete,
+    );
+    await completer.future;
+    return files;
+  }
+}
+
+extension FileSystemEntityExtension on FileSystemEntity {
+  /// Safely deletes a [FileSystemEntity].
+  FutureOr<void> delete_() async {
+    if (await exists_()) {
+      final prefix = Platform.isWindows ? r'\\?\' : '';
+      if (this is File) {
+        await File(prefix + path).delete();
+      } else if (this is Directory) {
+        await Directory(prefix + path).delete();
+      }
+    }
+  }
+
+  /// Safely checks whether a [FileSystemEntity] exists or not.
+  FutureOr<bool> exists_() {
+    final prefix = Platform.isWindows ? r'\\?\' : '';
+    if (this is File) {
+      return File(prefix + path).exists();
+    } else if (this is Directory) {
+      return Directory(prefix + path).exists();
+    } else {
+      return false;
+    }
+  }
+
+  /// Safely checks whether a [FileSystemEntity] exists or not.
+  bool existsSync_() {
+    final prefix = Platform.isWindows ? r'\\?\' : '';
+    if (this is File) {
+      return File(prefix + path).existsSync();
+    } else if (this is Directory) {
+      return Directory(prefix + path).existsSync();
+    } else {
+      return false;
+    }
+  }
+}
