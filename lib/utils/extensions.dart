74e420bd4743c838b803a85b39e79da8b995782f
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-03-21 17:17:51 +0000

    missing license header

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,3 +1,10 @@
+/// This file is a part of Harmonoid (https://github.com/harmonoid/harmonoid).
+///
+/// Copyright © 2020-2022, Yehuda Kremer <yehudakremer@gmail.com>.
+/// All rights reserved.
+///
+/// Use of this source code is governed by the End-User License Agreement for Harmonoid that can be found in the EULA.txt file.
+///
 extension IterableExtension<T> on Iterable<T> {
   /// Return distinct array by comparing hash codes.
   Iterable<T> distinct() {
