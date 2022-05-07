891fd72a6ba7bf6d7f712286076fc7aeed79e3e3
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-05-07 16:11:56 +0000

    bump project version to v0.2.3

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ import 'package:harmonoid/utils/argument_vector_handler.dart';
 import 'package:harmonoid/utils/override_window_destroy.dart';
 
 const String kTitle = 'Harmonoid';
-const String kVersion = 'v0.2.2';
+const String kVersion = 'v0.2.3';
 const String kAuthor = 'Hitesh Kumar Saini <saini123hitesh@gmail.com>';
 const String kLicense = 'End-User License Agreement for Harmonoid';
 
