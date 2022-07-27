97c5c76add96cb8965b32dd5a47d646c77bfcb62
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-07-27 20:16:16 +0530

    perf: rewrite android intent & platform channel handler

--- android/app/src/main/kotlin/com/alexmercerind/harmonoid/MainActivity.kt
+++ android/app/src/main/kotlin/com/alexmercerind/harmonoid/MainActivity.kt
@@ -1,174 +1,143 @@
+/// This file is a part of Harmonoid (https://github.com/harmonoid/harmonoid).
+///
+/// Copyright © 2020-2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+///
+/// Use of this source code is governed by the End-User License Agreement for Harmonoid that can be found in the EULA.txt file.
+///
+
 package com.alexmercerind.harmonoid
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import io.flutter.Log
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import android.content.Intent
-import android.os.Bundle
-import androidx.annotation.NonNull
-import android.annotation.SuppressLint
-import android.content.ContentUris
-import android.content.Context
-import android.content.CursorLoader
-import android.database.Cursor
-import android.net.Uri
-import android.os.Build
-import android.os.Environment
-import android.provider.DocumentsContract
-import android.provider.MediaStore
-import android.text.TextUtils
-import com.ryanheise.audioservice.AudioServiceActivity;
-
-
-object RealPathUtil {
-
-    fun getRealPath(context: Context, fileUri: Uri): String? {
-        return if (Build.VERSION.SDK_INT < 19) {
-            getRealPathFromURIAPI11to18(context, fileUri)
-        } else {
-            getRealPathFromURIAPI19(context, fileUri)
-        }
-    }
-
-    @SuppressLint("NewApi")
-    fun getRealPathFromURIAPI11to18(context: Context, contentUri: Uri): String? {
-        val proj = arrayOf(MediaStore.Images.Media.DATA)
-        var result: String? = null
-        val cursorLoader = CursorLoader(context, contentUri, proj, null, null, null)
-        val cursor = cursorLoader.loadInBackground()
-        if (cursor != null) {
-            val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-            cursor.moveToFirst()
-            result = cursor.getString(columnIndex)
-            cursor.close()
-        }
-        return result
-    }
-
-    @SuppressLint("NewApi")
-    fun getRealPathFromURIAPI19(context: Context, uri: Uri): String? {
-        val isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            if (isExternalStorageDocument(uri)) {
-                val docId = DocumentsContract.getDocumentId(uri)
-                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                val type = split[0]
-
-                if ("primary".equals(type, ignoreCase = true)) {
-                    return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
-                }
-            } else if (isDownloadsDocument(uri)) {
-                var cursor: Cursor? = null
-                try {
-                    cursor = context.contentResolver.query(uri, arrayOf(MediaStore.MediaColumns.DISPLAY_NAME), null, null, null)
-                    cursor!!.moveToNext()
-                    val fileName = cursor.getString(0)
-                    val path = Environment.getExternalStorageDirectory().toString() + "/Download/" + fileName
-                    if (!TextUtils.isEmpty(path)) {
-                        return path
-                    }
-                } finally {
-                    cursor?.close()
-                }
-                val id = DocumentsContract.getDocumentId(uri)
-                if (id.startsWith("raw:")) {
-                    return id.replaceFirst("raw:".toRegex(), "")
-                }
-                val contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads"), java.lang.Long.valueOf(id))
-                return getDataColumn(context, contentUri, null, null)
-            } else if (isMediaDocument(uri)) {
-                val docId = DocumentsContract.getDocumentId(uri)
-                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                val type = split[0]
-
-                var contentUri: Uri? = null
-                when (type) {
-                    "image" -> contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                    "video" -> contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-                    "audio" -> contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-                }
-                val selection = "_id=?"
-                val selectionArgs = arrayOf(split[1])
-                return getDataColumn(context, contentUri, selection, selectionArgs)
-            }
-        } else if ("content".equals(uri.scheme!!, ignoreCase = true)) {
-            return if (isGooglePhotosUri(uri)) uri.lastPathSegment else getDataColumn(context, uri, null, null)
-        } else if ("file".equals(uri.scheme!!, ignoreCase = true)) {
-            return uri.path
-        }
-        return null
-    }
-
-    private fun getDataColumn(context: Context, uri: Uri?, selection: String?,
-        selectionArgs: Array<String>?): String? {
-        var cursor: Cursor? = null
-        val column = "_data"
-        val projection = arrayOf(column)
-        try {
-            cursor = context.contentResolver.query(uri!!, projection, selection, selectionArgs, null)
-            if (cursor != null && cursor.moveToFirst()) {
-                val index = cursor.getColumnIndexOrThrow(column)
-                return cursor.getString(index)
-            }
-        } finally {
-            cursor?.close()
-        }
-        return null
-    }
-
-    private fun isExternalStorageDocument(uri: Uri): Boolean {
-        return "com.android.externalstorage.documents" == uri.authority
-    }
-
-    private fun isDownloadsDocument(uri: Uri): Boolean {
-        return "com.android.providers.downloads.documents" == uri.authority
-    }
-
-    private fun isMediaDocument(uri: Uri): Boolean {
-        return "com.android.providers.media.documents" == uri.authority
-    }
-
-    private fun isGooglePhotosUri(uri: Uri): Boolean {
-        return "com.google.android.apps.photos.content" == uri.authority
-    }
-}
+import java.util.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
+import com.ryanheise.audioservice.AudioServiceActivity
 
 
 class MainActivity : AudioServiceActivity() {
-    private val CHANNEL = "com.alexmercerind.harmonoid/open_file"
-    var openPath: String? = null
-    
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    private var channel: MethodChannel? = null
+    private var uri: String? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
-        val channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
-        channel.setMethodCallHandler { call, result ->
-            when (call.method) {
-                "getOpenFile" -> {
-                    result.success(openPath)
-                }
-                else -> result.notImplemented()
-            }
+        channel = MethodChannel(
+                flutterEngine.dartExecutor.binaryMessenger,
+                "com.alexmercerind.harmonoid"
+        )
+        channel?.setMethodCallHandler { _, result ->
+            result.success(uri)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        handleOpenFileUrl(intent)
+        receiveIntent(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        receiveIntent(intent)
     }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
-        handleOpenFileUrl(intent)
+        receiveIntent(intent)
     }
 
-    private fun handleOpenFileUrl(intent: Intent?) {
-        if (intent?.data != null) {
-            openPath = RealPathUtil.getRealPath(
-                getApplicationContext(),
-                intent?.data!!
-            )
+    @Synchronized
+    private fun receiveIntent(intent: Intent?) {
+        // This has been configured to work correctly with android:launchMode as singleTask.
+        if (intent != null) {
+            var result: String? = null
+            Log.d("MainActivity.java", intent.scheme.toString())
+            Log.d("MainActivity.java", intent.action.toString())
+            Log.d("MainActivity.java", intent.type.toString())
+            Log.d("MainActivity.java", intent.data.toString())
+            if (arrayListOf("file", "http").contains(intent.data?.scheme)) {
+                result = intent.data.toString()
+            }
+            // Separately handle modern content:// URIs in Android.
+            else if (intent.data?.scheme == "content") {
+                val resolveInfoList: List<ResolveInfo> = context.packageManager
+                        .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+                // Not requesting Intent.FLAG_GRANT_READ_URI_PERMISSION.
+                // Since, we are never going to write the file which is being opened.
+                //
+                // It requires different approach in new Android versions to write the opened file.
+                // Likely through ACTION_OPEN_DOCUMENT & FileDescriptor.
+                // Though, since this seems to work perfectly. It's good to go.
+                for (resolveInfo in resolveInfoList) {
+                    val packageName: String = resolveInfo.activityInfo.packageName
+                    context.grantUriPermission(
+                            packageName,
+                            intent.data, Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    )
+                }
+                // In general, the idea is to copy the file to a local temporary file with unique
+                // URI received through intent, inside app's cache directory.
+                //
+                // Get reference to the InputStream from this content:// URI.
+                val inputStream: InputStream? = contentResolver.openInputStream(intent.data!!)
+                // The cache directory of the application. Equivalent to getExternalStorageDirectory
+                // in package:path_provider of Flutter.
+                var externalFilesDirAbsolutePath = context.getExternalFilesDir(null)?.absolutePath!!
+                if (externalFilesDirAbsolutePath.endsWith("/")) {
+                    externalFilesDirAbsolutePath = externalFilesDirAbsolutePath.substring(
+                            0, externalFilesDirAbsolutePath.length - 1
+                    )
+                }
+                // Saving the file in "Intents" subdirectory, for easy clean-up the cache in future.
+                val intentFilesDirAbsolutePath = "$externalFilesDirAbsolutePath/Intents"
+                Log.d("MainActivity.java", intentFilesDirAbsolutePath)
+                // Last segment of the URI is interpreted as the file path.
+                val fileName = intent.data.toString().split("/").toList().lastOrNull()
+                Log.d("MainActivity.java", fileName.toString())
+                // If file name is null, then a random integer value is used as the temporary file's
+                // name. The file name is then parsed using URLDecode.decode, which removes any of
+                // ambiguity that can be caused by parsing the final URI by Dart's Uri.parse.
+                // Since, decoding of the URI component may result in some illegal file path
+                // characters, they are later on removed using a simple regex.
+                val path = "$intentFilesDirAbsolutePath/${
+                    URLDecoder.decode(
+                            fileName ?: Random().nextInt(Integer.MAX_VALUE).toString(),
+                            StandardCharsets.UTF_8.toString()
+                    ).replace("[\\\\/:*?\"<>| ]".toRegex(), "")
+                }"
+                Log.d("MainActivity.java", path)
+                // Delete the directory where the temporary files are placed. This is because
+                // some previous intent handling would've resulted in file creations here.
+                // This wastage of space can quickly get out of hands.
+                if (File(intentFilesDirAbsolutePath).exists()
+                        && File(intentFilesDirAbsolutePath).isDirectory) {
+                    File(intentFilesDirAbsolutePath).deleteRecursively()
+                }
+                // Recursively create all the directories & subdirectories to the temporary file,
+                // and copy the stream to it after creation of file itself.
+                File(intentFilesDirAbsolutePath).mkdirs()
+                if (!File(path).exists()) {
+                    File(path).createNewFile()
+                }
+                inputStream?.copyTo(FileOutputStream(path))
+                result = "file://$path"
+                Log.d("MainActivity.java", path)
+            }
+            if (!arrayListOf(null, uri).contains(result)) {
+                // Notify the newly opened file through the platform channel.
+                uri = result
+                channel?.invokeMethod("MainActivity.java", uri)
+                Log.d("MainActivity.java", uri.toString())
+            }
         }
     }
 }
