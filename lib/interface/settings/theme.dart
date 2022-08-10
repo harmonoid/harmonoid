2c4e3ccdfc8c0bce5595fcaeb378fb1892681f87
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-08-11 00:04:18 +0530

    style: tweak mobile settings ui

--- lib/interface/settings/theme.dart
+++ lib/interface/settings/theme.dart
@@ -8,65 +8,161 @@
 
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:harmonoid/utils/rendering.dart';
 import 'package:provider/provider.dart';
 
 import 'package:harmonoid/interface/settings/settings.dart';
+import 'package:harmonoid/utils/rendering.dart';
 import 'package:harmonoid/state/visuals.dart';
 import 'package:harmonoid/constants/language.dart';
 
 class ThemeSetting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SettingsTile(
-        title: Language.instance.SETTING_THEME_TITLE,
-        subtitle: Language.instance.SETTING_THEME_SUBTITLE,
-        child: Consumer<Visuals>(
-          builder: (context, visuals, _) => Column(
-            children: [
-              if (Platform.isAndroid || Platform.isIOS || Platform.isWindows)
-                RadioListTile(
-                  value: ThemeMode.system,
-                  title: Text(
-                    Language.instance.THEME_MODE_SYSTEM,
-                    style: isDesktop
-                        ? Theme.of(context).textTheme.headline4
-                        : null,
+    return isDesktop
+        ? SettingsTile(
+            title: Language.instance.SETTING_THEME_TITLE,
+            subtitle: Language.instance.SETTING_THEME_SUBTITLE,
+            child: Consumer<Visuals>(
+              builder: (context, visuals, _) => Column(
+                children: [
+                  if (Platform.isAndroid ||
+                      Platform.isIOS ||
+                      Platform.isWindows)
+                    RadioListTile(
+                      value: ThemeMode.system,
+                      title: Text(
+                        Language.instance.THEME_MODE_SYSTEM,
+                        style: isDesktop
+                            ? Theme.of(context).textTheme.headline4
+                            : null,
+                      ),
+                      groupValue: visuals.themeMode,
+                      onChanged: (dynamic themeMode) => visuals.update(
+                        themeMode: themeMode,
+                        context: context,
+                      ),
+                    ),
+                  RadioListTile(
+                    value: ThemeMode.light,
+                    title: Text(
+                      Language.instance.THEME_MODE_LIGHT,
+                      style: isDesktop
+                          ? Theme.of(context).textTheme.headline4
+                          : null,
+                    ),
+                    groupValue: visuals.themeMode,
+                    onChanged: (dynamic themeMode) => visuals.update(
+                      themeMode: themeMode,
+                      context: context,
+                    ),
                   ),
-                  groupValue: visuals.themeMode,
-                  onChanged: (dynamic themeMode) => visuals.update(
-                    themeMode: themeMode,
-                    context: context,
+                  RadioListTile(
+                    value: ThemeMode.dark,
+                    title: Text(
+                      Language.instance.THEME_MODE_DARK,
+                      style: isDesktop
+                          ? Theme.of(context).textTheme.headline4
+                          : null,
+                    ),
+                    groupValue: visuals.themeMode,
+                    onChanged: (dynamic themeMode) => visuals.update(
+                      themeMode: themeMode,
+                      context: context,
+                    ),
                   ),
-                ),
-              RadioListTile(
-                value: ThemeMode.light,
-                title: Text(
-                  Language.instance.THEME_MODE_LIGHT,
-                  style:
-                      isDesktop ? Theme.of(context).textTheme.headline4 : null,
-                ),
-                groupValue: visuals.themeMode,
-                onChanged: (dynamic themeMode) => visuals.update(
-                  themeMode: themeMode,
-                  context: context,
-                ),
+                ],
               ),
-              RadioListTile(
-                value: ThemeMode.dark,
-                title: Text(
-                  Language.instance.THEME_MODE_DARK,
-                  style:
-                      isDesktop ? Theme.of(context).textTheme.headline4 : null,
-                ),
-                groupValue: visuals.themeMode,
-                onChanged: (dynamic themeMode) => visuals.update(
-                  themeMode: themeMode,
+            ),
+          )
+        : Consumer<Visuals>(
+            builder: (context, visuals, _) => ListTile(
+              onTap: () async {
+                ThemeMode value = visuals.themeMode;
+                await showDialog(
                   context: context,
-                ),
+                  builder: (context) => AlertDialog(
+                    title: Text(Language.instance.SETTING_THEME_TITLE),
+                    contentPadding: EdgeInsets.only(top: 20.0),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Divider(
+                          height: 1.0,
+                          thickness: 1.0,
+                        ),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxHeight: 420.0,
+                          ),
+                          child: StatefulBuilder(
+                            builder: (context, setState) =>
+                                SingleChildScrollView(
+                              child: Column(
+                                children: ThemeMode.values
+                                    .map(
+                                      (e) => RadioListTile<ThemeMode>(
+                                        title: Text(
+                                          {
+                                            ThemeMode.system: Language
+                                                .instance.THEME_MODE_SYSTEM,
+                                            ThemeMode.light: Language
+                                                .instance.THEME_MODE_LIGHT,
+                                            ThemeMode.dark: Language
+                                                .instance.THEME_MODE_DARK,
+                                          }[e]!,
+                                        ),
+                                        groupValue: value,
+                                        onChanged: (e) {
+                                          if (e != null) {
+                                            setState(() => value = e);
+                                          }
+                                        },
+                                        value: e,
+                                      ),
+                                    )
+                                    .toList(),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Divider(
+                          height: 1.0,
+                          thickness: 1.0,
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () async {
+                          Navigator.of(context).maybePop();
+                          visuals.update(
+                            themeMode: value,
+                            context: context,
+                          );
+                        },
+                        child: Text(
+                          Language.instance.OK,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: Navigator.of(context).maybePop,
+                        child: Text(
+                          Language.instance.CANCEL,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              title: Text(Language.instance.SETTING_THEME_TITLE),
+              subtitle: Text(
+                {
+                  ThemeMode.system: Language.instance.THEME_MODE_SYSTEM,
+                  ThemeMode.light: Language.instance.THEME_MODE_LIGHT,
+                  ThemeMode.dark: Language.instance.THEME_MODE_DARK,
+                }[visuals.themeMode]!,
               ),
-            ],
-          ),
-        ));
+            ),
+          );
   }
 }
