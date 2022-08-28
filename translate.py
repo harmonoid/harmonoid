fb17029666c399fe02b5bd2070023610643873b9
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-08-29 00:28:58 +0530

    build(utils): update translation scripts

--- translate.py
+++ translate.py
@@ -8,65 +8,69 @@ if __name__ == "__main__":
     if input(f"Add {string} = {value}?\n") in ["y", "Y"]:
         file_names = os.listdir("assets/translations")
         for file_name in file_names:
-            with open(
-                f"assets/translations/{file_name}",
-                "r+",
-                encoding="utf_8",
-                errors="ignore",
-            ) as file:
-                language = dict(json.loads(file.read()))
-                language[string] = value
-                keys = list(language.keys())
+            if ".json" in file_name:
                 with open(
                     f"assets/translations/{file_name}",
-                    "w",
+                    "r+",
+                    encoding="utf_8",
+                    errors="ignore",
+                ) as file:
+                    language = dict(json.loads(file.read()))
+                    language[string] = value
+                    keys = list(language.keys())
+                    with open(
+                        f"assets/translations/{file_name}",
+                        "w",
+                        encoding="utf_8",
+                        errors="ignore",
+                    ) as writeable:
+                        writeable.write(
+                            json.dumps(
+                                dict(sorted(language.items())),
+                                indent=2,
+                                ensure_ascii=False,
+                            )
+                            + "\n"
+                        )
+            keys.sort()
+            with open(
+                "lib/constants/strings.dart", "w", encoding="utf_8", errors="ignore"
+            ) as file:
+                file.write(
+                    """// ignore_for_file: non_constant_identifier_names
+
+        class Strings {
+        """
+                )
+                for key in keys:
+                    file.write(f"  late String {key};\n")
+                file.write("}\n")
+            with open(
+                "lib/constants/language.dart", "r+", encoding="utf_8", errors="ignore"
+            ) as file:
+                contents = file.read()
+                file.close()
+                with open(
+                    "lib/constants/language.dart",
+                    "r+",
                     encoding="utf_8",
                     errors="ignore",
                 ) as writeable:
                     writeable.write(
-                        json.dumps(
-                            dict(sorted(language.items())),
-                            indent=2,
-                            ensure_ascii=False,
-                        )
-                        + "\n"
+                        contents.split("var asset = jsonDecode(string);")[0]
+                        + "var asset = jsonDecode(string);\n"
                     )
-        keys.sort()
-        with open(
-            "lib/constants/strings.dart", "w", encoding="utf_8", errors="ignore"
-        ) as file:
-            file.write(
-                """// ignore_for_file: non_constant_identifier_names
-
-class Strings {
-"""
-            )
-            for key in keys:
-                file.write(f"  late String {key};\n")
-            file.write("}\n")
-        with open(
-            "lib/constants/language.dart", "r+", encoding="utf_8", errors="ignore"
-        ) as file:
-            contents = file.read()
-            file.close()
-            with open(
-                "lib/constants/language.dart", "r+", encoding="utf_8", errors="ignore"
-            ) as writeable:
-                writeable.write(
-                    contents.split("var asset = jsonDecode(string);")[0]
-                    + "var asset = jsonDecode(string);\n"
-                )
-                for key in keys:
-                    writeable.write(f"    this.{key} = asset['{key}']!;\n")
-                writeable.write(
-                    """    Configuration.instance.save(languageRegion: languageRegion);
-    this.current = languageRegion;
-    this.notifyListeners();
-  }
-  late LanguageRegion current;
-  @override
-  // ignore: must_call_super
-  void dispose() {}
-}
-"""
-                )
+                    for key in keys:
+                        writeable.write(f"    this.{key} = asset['{key}']!;\n")
+                    writeable.write(
+                        """    Configuration.instance.save(languageRegion: languageRegion);
+            this.current = languageRegion;
+            this.notifyListeners();
+        }
+        late LanguageRegion current;
+        @override
+        // ignore: must_call_super
+        void dispose() {}
+        }
+        """
+                    )
