f2465cf76414525df03130d9db9093589610d7f2
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-09-13 19:04:08 +0530

    feat: redraw StatsSetting on changes

--- lib/interface/settings/stats.dart
+++ lib/interface/settings/stats.dart
@@ -6,11 +6,12 @@
 /// Use of this source code is governed by the End-User License Agreement for Harmonoid that can be found in the EULA.txt file.
 ///
 import 'package:flutter/material.dart';
-import 'package:harmonoid/constants/language.dart';
-import 'package:harmonoid/core/collection.dart';
+import 'package:provider/provider.dart';
 
+import 'package:harmonoid/core/collection.dart';
 import 'package:harmonoid/interface/settings/settings.dart';
 import 'package:harmonoid/utils/rendering.dart';
+import 'package:harmonoid/constants/language.dart';
 
 class StatsSetting extends StatefulWidget {
   StatsSetting({Key? key}) : super(key: key);
@@ -42,64 +43,66 @@ class Table extends StatelessWidget {
       margin: isDesktop
           ? EdgeInsets.only(top: tileMargin)
           : EdgeInsets.symmetric(vertical: 2 * tileMargin),
-      child: DataTable(
-        headingRowHeight: 44.0,
-        dataRowHeight: 44.0,
-        columnSpacing: 0.0,
-        columns: [
-          DataColumn(
-            label: Container(
-              width: 56.0,
-              child: Text(''),
+      child: Consumer<Collection>(
+        builder: (context, collection, _) => DataTable(
+          headingRowHeight: 44.0,
+          dataRowHeight: 44.0,
+          columnSpacing: 0.0,
+          columns: [
+            DataColumn(
+              label: Container(
+                width: 56.0,
+                child: Text(''),
+              ),
             ),
-          ),
-          DataColumn(
-            label: Container(
-              width: isDesktop
-                  ? 160.0
-                  : (MediaQuery.of(context).size.width - 56.0) / 2,
-              child: Text(Language.instance.TYPE),
+            DataColumn(
+              label: Container(
+                width: isDesktop
+                    ? 160.0
+                    : (MediaQuery.of(context).size.width - 56.0) / 2,
+                child: Text(Language.instance.TYPE),
+              ),
             ),
-          ),
-          DataColumn(
-            label: Container(
-              width: isDesktop
-                  ? 60.0
-                  : (MediaQuery.of(context).size.width - 56.0) / 2,
-              child: Text(Language.instance.COUNT),
+            DataColumn(
+              label: Container(
+                width: isDesktop
+                    ? 60.0
+                    : (MediaQuery.of(context).size.width - 56.0) / 2,
+                child: Text(Language.instance.COUNT),
+              ),
             ),
-          ),
-        ],
-        rows: [
-          DataRow(
-            cells: [
-              DataCell(Icon(Icons.music_note)),
-              DataCell(Text(Language.instance.TRACK)),
-              DataCell(Text(Collection.instance.tracks.length.toString())),
-            ],
-          ),
-          DataRow(
-            cells: [
-              DataCell(Icon(Icons.album)),
-              DataCell(Text(Language.instance.ALBUM)),
-              DataCell(Text(Collection.instance.albums.length.toString())),
-            ],
-          ),
-          DataRow(
-            cells: [
-              DataCell(Icon(Icons.person)),
-              DataCell(Text(Language.instance.ARTIST)),
-              DataCell(Text(Collection.instance.artists.length.toString())),
-            ],
-          ),
-          DataRow(
-            cells: [
-              DataCell(Icon(Icons.piano)),
-              DataCell(Text(Language.instance.GENRE)),
-              DataCell(Text(Collection.instance.genres.length.toString())),
-            ],
-          ),
-        ],
+          ],
+          rows: [
+            DataRow(
+              cells: [
+                DataCell(Icon(Icons.music_note)),
+                DataCell(Text(Language.instance.TRACK)),
+                DataCell(Text(collection.tracks.length.toString())),
+              ],
+            ),
+            DataRow(
+              cells: [
+                DataCell(Icon(Icons.album)),
+                DataCell(Text(Language.instance.ALBUM)),
+                DataCell(Text(collection.albums.length.toString())),
+              ],
+            ),
+            DataRow(
+              cells: [
+                DataCell(Icon(Icons.person)),
+                DataCell(Text(Language.instance.ARTIST)),
+                DataCell(Text(collection.artists.length.toString())),
+              ],
+            ),
+            DataRow(
+              cells: [
+                DataCell(Icon(Icons.piano)),
+                DataCell(Text(Language.instance.GENRE)),
+                DataCell(Text(collection.genres.length.toString())),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
