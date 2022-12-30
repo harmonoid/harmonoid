732b89e5d2d25d1f815dfce919cc4a68a7c08320
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-12-30 21:08:11 +0530

    refactor(android): MetadataRetriever impl

--- android/app/src/main/kotlin/com/alexmercerind/harmonoid/MetadataRetriever.java
+++ android/app/src/main/kotlin/com/alexmercerind/harmonoid/MetadataRetriever.java
@@ -34,16 +34,13 @@ import android.media.MediaMetadataRetriever;
 import android.webkit.MimeTypeMap;
 
 class MediaMetadataRetrieverExt extends MediaMetadataRetriever {
-    private final String uri;
-
-    public MediaMetadataRetrieverExt(String uri) {
+    public MediaMetadataRetrieverExt() {
         super();
-        this.uri = uri;
     }
 
     // Just for preventing any [Exception] when reading a metadata key.
     // Better is just to avoid, instead of crashing.
-    public String readKey(int keyCode) {
+    private String readKey(int keyCode) {
         try {
             return extractMetadata(keyCode);
         } catch (Exception e) {
@@ -52,75 +49,29 @@ class MediaMetadataRetrieverExt extends MediaMetadataRetriever {
         }
     }
 
-    public HashMap<String, Object> getMetadata() {
-        HashMap<String, Object> metadata = new HashMap<>();
-        metadata.put("uri", uri);
-        metadata.put("trackName", readKey(METADATA_KEY_TITLE));
-        metadata.put("trackArtistNames", readKey(METADATA_KEY_ARTIST));
-        metadata.put("albumName", readKey(METADATA_KEY_ALBUM));
-        metadata.put("albumArtistName", readKey(METADATA_KEY_ALBUMARTIST));
-        // Read [trackNumber] & [albumLength] from the passed source.
-        final String trackNumber = readKey(METADATA_KEY_CD_TRACK_NUMBER);
-        final String albumLength = readKey(METADATA_KEY_NUM_TRACKS);
-        try {
-            // If [albumLength] is non-null, insert into the final [metadata] [HashMap].
-            if (albumLength != null) {
-                metadata.put("albumLength", albumLength);
-            }
-            // Operate over [trackNumber] key, if it is non-null.
-            if (trackNumber != null) {
-                final String[] trackNumberData = trackNumber.split("/");
-                // Some MP3s store [trackNumber] as `4/20`. Split across `/` & store first section
-                // as [trackNumber] inside the final metadata [HashMap].
-                metadata.put("trackNumber", trackNumberData[0].trim());
-                // If [albumLength] was not already put inside [metadata] from
-                // [METADATA_KEY_NUM_TRACKS] and we found more than one element(s) in
-                // [trackNumberSplit], then use the last section from the [trackNumber] as
-                // [albumLength].
-                if (!metadata.containsKey("albumLength")) {
-                    if (trackNumberData.length > 1) {
-                        metadata.put(
-                                "albumLength",
-                                trackNumberData[trackNumberData.length - 1].trim()
-                        );
-                    } else {
-                        metadata.put("albumLength", null);
-                    }
-                }
-            }
-        } catch (Exception exception) {
-            metadata.put("trackNumber", null);
-            metadata.put("albumLength", null);
-        }
-        final String discNumber = readKey(METADATA_KEY_DISC_NUMBER);
-        try {
-            if (discNumber != null) {
-                final String[] discNumberData = discNumber.split("/");
-                metadata.put("discNumber", discNumberData[0]);
-            }
-        } catch (Exception exception) {
-            metadata.put("discNumber", null);
-        }
-        final String year = readKey(METADATA_KEY_YEAR);
-        final String date = readKey(METADATA_KEY_DATE);
-        try {
-            metadata.put("year", year.trim());
-        } catch (Exception exception) {
-            try {
-                // If year is not found (will throw [NullPointerException]), then use the date tag
-                // to extract year.
-                // Generally, year tag seems to stored as 2002.07.23, 2002/07/23 or 2002-07-23.
-                metadata.put("year", date.split("[.\\-/]")[0].trim());
-            } catch (Exception e) {
-                metadata.put("year", null);
-            }
-        }
-        metadata.put("genre", readKey(METADATA_KEY_GENRE));
-        metadata.put("authorName", readKey(METADATA_KEY_AUTHOR));
-        metadata.put("writerName", readKey(METADATA_KEY_WRITER));
-        metadata.put("mimeType", readKey(METADATA_KEY_MIMETYPE));
-        metadata.put("duration", readKey(METADATA_KEY_DURATION));
-        metadata.put("bitrate", readKey(METADATA_KEY_BITRATE));
+    public HashMap<String, String> read() {
+        HashMap<String, String> metadata = new HashMap<>();
+        metadata.put("METADATA_KEY_CD_TRACK_NUMBER", readKey(METADATA_KEY_CD_TRACK_NUMBER));
+        metadata.put("METADATA_KEY_ALBUM", readKey(METADATA_KEY_ALBUM));
+        metadata.put("METADATA_KEY_ARTIST", readKey(METADATA_KEY_ARTIST));
+        metadata.put("METADATA_KEY_AUTHOR", readKey(METADATA_KEY_AUTHOR));
+        metadata.put("METADATA_KEY_COMPOSER", readKey(METADATA_KEY_COMPOSER));
+        metadata.put("METADATA_KEY_DATE", readKey(METADATA_KEY_DATE));
+        metadata.put("METADATA_KEY_GENRE", readKey(METADATA_KEY_GENRE));
+        metadata.put("METADATA_KEY_TITLE", readKey(METADATA_KEY_TITLE));
+        metadata.put("METADATA_KEY_YEAR", readKey(METADATA_KEY_YEAR));
+        metadata.put("METADATA_KEY_DURATION", readKey(METADATA_KEY_DURATION));
+        metadata.put("METADATA_KEY_NUM_TRACKS", readKey(METADATA_KEY_NUM_TRACKS));
+        metadata.put("METADATA_KEY_WRITER", readKey(METADATA_KEY_WRITER));
+        metadata.put("METADATA_KEY_MIMETYPE", readKey(METADATA_KEY_MIMETYPE));
+        metadata.put("METADATA_KEY_ALBUMARTIST", readKey(METADATA_KEY_ALBUMARTIST));
+        metadata.put("METADATA_KEY_DISC_NUMBER", readKey(METADATA_KEY_DISC_NUMBER));
+        metadata.put("METADATA_KEY_COMPILATION", readKey(METADATA_KEY_COMPILATION));
+        metadata.put("METADATA_KEY_HAS_AUDIO", readKey(METADATA_KEY_HAS_AUDIO));
+        metadata.put("METADATA_KEY_HAS_VIDEO", readKey(METADATA_KEY_HAS_VIDEO));
+        metadata.put("METADATA_KEY_VIDEO_WIDTH", readKey(METADATA_KEY_VIDEO_WIDTH));
+        metadata.put("METADATA_KEY_VIDEO_HEIGHT", readKey(METADATA_KEY_VIDEO_HEIGHT));
+        metadata.put("METADATA_KEY_BITRATE", readKey(METADATA_KEY_BITRATE));
         Log.d("Harmonoid", metadata.toString());
         return metadata;
     }
@@ -129,10 +80,7 @@ class MediaMetadataRetrieverExt extends MediaMetadataRetriever {
 public class MetadataRetriever implements MethodCallHandler {
     @SuppressWarnings("UnusedDeclaration")
     @Override
-    public void onMethodCall(
-            @NonNull final MethodCall call,
-            @NonNull final Result result
-    ) {
+    public void onMethodCall(@NonNull final MethodCall call, @NonNull final Result result) {
         // Passed [uri] inside the arguments must be a String interpretation of a URI, which follows
         // a scheme such as `file://` or `http://` etc. Where as, [coverDirectory] must be a direct
         // path to the file system directory where the cover art will be extracted (not a URI).
@@ -149,37 +97,35 @@ public class MetadataRetriever implements MethodCallHandler {
             // Run [MediaMetadataRetriever] on another thread. Extracting metadata of a [File] is
             // a heavy operation & causes substantial jitter in the UI.
             CompletableFuture.runAsync(() -> {
-                final MediaMetadataRetrieverExt retriever = new MediaMetadataRetrieverExt(uri[0]);
+                final MediaMetadataRetrieverExt retriever = new MediaMetadataRetrieverExt();
                 // Try to get the [FileInputStream] from the passed [uri].
                 try {
                     // Only used for `file://` scheme.
                     FileInputStream input = null;
-                    HashMap<String, Object> metadata;
+                    HashMap<String, String> metadata = new HashMap<String, String>();
                     // Handle `file://`.
                     if (uri[0].toLowerCase().startsWith("file://")) {
                         input = new FileInputStream(Uri.parse(uri[0]).getPath());
                         // Set data source using [FileDescriptor], which tends to be safer.
                         retriever.setDataSource(input.getFD());
-                        metadata = retriever.getMetadata();
-                        metadata.put("uri", uri[0]);
+                        metadata = retriever.read();
                         // Return the metadata.
-                        final HashMap<String, Object> response = metadata;
+                        final HashMap<String, String> response = metadata;
                         if (!waitUntilAlbumArtIsSaved[0]) {
                             new Handler(Looper.getMainLooper()).post(() -> result.success(response));
                         }
                     } else {
                         // Handle other URI schemes. Expected to be network URLs. Hope for the best!
                         retriever.setDataSource(uri[0], new HashMap<>());
-                        metadata = retriever.getMetadata();
-                        metadata.put("uri", uri[0]);
-                        final HashMap<String, Object> response = metadata;
+                        metadata = retriever.read();
+                        final HashMap<String, String> response = metadata;
                         if (!waitUntilAlbumArtIsSaved[0]) {
                             new Handler(Looper.getMainLooper()).post(() -> result.success(response));
                         }
                     }
                     // Now proceed to save the album art in background.
-                    String trackName = (String) metadata.get("trackName");
-                    // See [Track] model within Harmonoid.
+                    String trackName = metadata.get("METADATA_KEY_TITLE");
+                    // NOTE: Following same contract as Harmonoid. Only used to save the album art locally.
                     if (trackName == null) {
                         // If [trackName] is found null, we extract a human-understandable [String]
                         // from the [File]'s original name.
@@ -190,43 +136,39 @@ public class MetadataRetriever implements MethodCallHandler {
                         // Split across last `/` in the URI & keep it as [trackName].
                         trackName = uri[0].split("/")[uri[0].split("/").length - 1];
                         // We need to decode the URI component to get actual [File]'s name.
-                        // Only done for `file://` scheme URIs. See Harmonoid's Track fromJson
-                        // factory constructor for more information.
+                        // Only done for `file://` scheme URIs. See Harmonoid's Track fromJson factory constructor for more information.
                         if (uri[0].toLowerCase().startsWith("file://")) {
                             try {
-                                trackName = URLDecoder.decode(
-                                        trackName,
-                                        StandardCharsets.UTF_8.toString()
-                                );
+                                trackName = URLDecoder.decode(trackName, StandardCharsets.UTF_16.toString());
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
                         }
                     }
-                    // See [Track] model within Harmonoid.
-                    String albumName = (String) metadata.get("albumName");
+                    String albumName = metadata.get("METADATA_KEY_ALBUM");
                     if (albumName == null) {
                         albumName = "Unknown Album";
                     }
-                    String albumArtistName = (String) metadata.get("albumArtistName");
+                    String albumArtistName = metadata.get("METADATA_KEY_ALBUMARTIST");
                     if (albumArtistName == null) {
                         albumArtistName = "Unknown Artist";
                     }
                     // Remove trailing `/` from [coverDirectory] path.
                     if (coverDirectory[0].endsWith("/")) {
-                        coverDirectory[0] = coverDirectory[0].substring(
-                                0,
-                                coverDirectory[0].length() - 1
-                        );
+                        coverDirectory[0] = coverDirectory[0].substring(0, coverDirectory[0].length() - 1);
                     }
                     // Final album art [File].
                     final File file = new File(
                             String.format(
                                     "%s/%s.PNG",
                                     coverDirectory[0],
-                                    // See [albumArtFileName] getter on [Track] inside Harmonoid and
-                                    // [kAlbumArtFileNameRegex].
-                                    (trackName + albumName + albumArtistName).replaceAll(
+                                    // See [albumArtFileName] getter on [Track] inside Harmonoid and [kAlbumArtFileNameRegex].
+                                    String.format(
+                                            "%s%s%s",
+                                            trackName,
+                                            albumName,
+                                            albumArtistName
+                                    ).replaceAll(
                                             "[\\\\/:*?\"<>| ]"
                                             , ""
                                     )
@@ -262,16 +204,15 @@ public class MetadataRetriever implements MethodCallHandler {
                         e.printStackTrace();
                     }
                     if (waitUntilAlbumArtIsSaved[0]) {
-                        new Handler(Looper.getMainLooper()).post(() -> result.success(metadata));
+                        HashMap<String, String> response = metadata;
+                        new Handler(Looper.getMainLooper()).post(() -> result.success(response));
                     }
                 }
                 // Return fallback [metadata] [HashMap], with only [uri] key present inside it.
                 // In case a [FileNotFoundException] or [IOException] was thrown.
                 catch (Exception exception) {
                     exception.printStackTrace();
-                    final HashMap<String, Object> metadata = new HashMap<>();
-                    metadata.put("uri", uri[0]);
-                    new Handler(Looper.getMainLooper()).post(() -> result.success(metadata));
+                    new Handler(Looper.getMainLooper()).post(() -> result.success(new HashMap<String, String>()));
                 }
             });
         } else if (call.method.equals("format")) {
@@ -281,7 +222,7 @@ public class MetadataRetriever implements MethodCallHandler {
             if (uri != null && uri.toLowerCase().startsWith("file://")) {
                 CompletableFuture.runAsync(() -> {
                     MediaExtractor extractor = null;
-                    FileInputStream input;
+                    FileInputStream input = null;
                     try {
                         extractor = new MediaExtractor();
                         // Set data source using [FileDescriptor], which tends to be safer.
@@ -349,8 +290,12 @@ public class MetadataRetriever implements MethodCallHandler {
                     } catch (Exception e) {
                         e.printStackTrace();
                         new Handler(Looper.getMainLooper()).post(() -> result.success(response));
-                        if (extractor != null) {
-                            extractor.release();
+                        try {
+                            if (extractor != null) {
+                                extractor.release();
+                            }
+                        } catch (Exception exception) {
+                            exception.printStackTrace();
                         }
                     }
                 });
