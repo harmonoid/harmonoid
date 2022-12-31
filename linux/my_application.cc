310c5fb2a12aad8393e88d869cb86ea6b9d67ed6
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-12-31 23:52:17 +0530

    fix: do not use GTK headerbar (#380)

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -51,7 +51,8 @@ static void my_application_window_new(GApplication* application) {
   // in case the window manager does more exotic layout, e.g. tiling.
   // If running on Wayland assume the header bar will work (may need changing
   // if future cases occur).
-  gboolean use_header_bar = TRUE;
+  // Manually disabling GTK header bar. See: #380
+  gboolean use_header_bar = FALSE;
 #ifdef GDK_WINDOWING_X11
   GdkScreen* screen = gtk_window_get_screen(window);
   if (GDK_IS_X11_SCREEN(screen)) {
