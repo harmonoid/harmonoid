78b2ffb60359033451434587aefe019b13ac01ac
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-02-11 00:44:57 +0530

    feat: Visuals class

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart' hide Intent;
 import 'package:flutter/foundation.dart';
+import 'package:harmonoid/state/visuals.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:window_plus/window_plus.dart';
 import 'package:harmonoid/utils/android_tag_reader.dart';
@@ -74,6 +75,12 @@ Future<void> main(List<String> args) async {
         await SystemMediaTransportControls.initialize();
       }
       await Intent.initialize(args: args);
+      // TODO(@alexmercerind): Restore from application cache.
+      await Visuals.initialize(
+        standard: 3,
+        themeMode: Configuration.instance.themeMode,
+        systemColorScheme: true,
+      );
       DiscordRPC.initialize();
     }
     if (Platform.isLinux) {
@@ -90,6 +97,12 @@ Future<void> main(List<String> args) async {
       await NowPlayingVisuals.initialize();
       await Intent.initialize(args: args);
       DiscordRPC.initialize();
+      // TODO(@alexmercerind): Restore from application cache.
+      await Visuals.initialize(
+        standard: 3,
+        themeMode: Configuration.instance.themeMode,
+        systemColorScheme: true,
+      );
     }
     if (Platform.isAndroid) {
       await StorageRetriever.initialize();
@@ -97,11 +110,15 @@ Future<void> main(List<String> args) async {
         DeviceOrientation.portraitUp,
         DeviceOrientation.portraitDown,
       ]);
-      SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-        systemNavigationBarColor: Colors.black,
-        systemNavigationBarDividerColor: Colors.black,
-        systemNavigationBarIconBrightness: Brightness.dark,
-      ));
+      // Based on Material 2 specifications.
+      // Edge to edge based Material 3 configuration (if enabled by user) may be enabled during runtime later on.
+      SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(
+          systemNavigationBarColor: Colors.black,
+          systemNavigationBarDividerColor: Colors.black,
+          systemNavigationBarIconBrightness: Brightness.dark,
+        ),
+      );
       // Android 12 or lower.
       if (StorageRetriever.instance.version < 33) {
         if (await Permission.storage.isDenied ||
@@ -135,6 +152,12 @@ Future<void> main(List<String> args) async {
       await ExternalMediaProvider.create();
       await AppState.initialize();
       await Intent.initialize();
+      // TODO(@alexmercerind): Restore from application cache.
+      await Visuals.initialize(
+        standard: 3,
+        themeMode: Configuration.instance.themeMode,
+        systemColorScheme: true,
+      );
     }
     await Collection.initialize(
       collectionDirectories: Configuration.instance.collectionDirectories,
--- lib/state/visuals.dart
+++ lib/state/visuals.dart
@@ -5,77 +5,250 @@
 ///
 /// Use of this source code is governed by the End-User License Agreement for Harmonoid that can be found in the EULA.txt file.
 ///
+// ignore_for_file: implementation_imports
 
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:dynamic_color/src/dynamic_color_plugin.dart';
+import 'package:dynamic_color/src/corepalette_to_colorscheme.dart';
+
 import 'package:harmonoid/utils/theme.dart';
-import 'package:harmonoid/utils/rendering.dart';
 import 'package:harmonoid/core/configuration.dart';
 
-/// Default light theme color.
-const kPrimaryLightColor = Color(0xFF6200EA);
+/// Default color scheme for light theme in Material Design 3.
+final defaultLightColorSchemeM3 = ColorScheme.fromSeed(
+  seedColor: const Color(0xFF6750A4),
+  brightness: Brightness.light,
+);
 
-/// Default dark theme color.
-const kPrimaryDarkColor = Color(0xFF7C4DFF);
+/// Default color scheme for dark theme in Material Design 3.
+final defaultDarkColorSchemeM3 = ColorScheme.fromSeed(
+  seedColor: const Color(0xFF6750A4),
+  brightness: Brightness.dark,
+);
+
+/// Default light theme color in Material Design 2.
+const kDefaultLightPrimaryColorM2 = Color(0xFF651FFF);
+
+/// Default dark theme color in Material Design 2.
+const kDefaultDarkPrimaryColorM2 = Color(0xFF7C4DFF);
 
 /// Visuals
 /// -------
 ///
-/// Minimal [ChangeNotifier] which serves the purpose of passing the current
-/// app theme down the [Widget] tree & trigger UI updates whenever theme is
-/// changed, while automatically updating [SystemChrome] & saving current
-/// theme to the [Configuration].
+/// A minimal [ChangeNotifier] which serves the purpose of passing the current application
+/// theme down the [Widget] tree trigger redraws whenever theme is changed by the user or
+/// system, while automatically updating [SystemChrome] & saving current theme to the
+/// application's configuration cache.
+///
+/// * A single primary [light] or [dark] color is consumed in Material Design 2. It only works if [material3] is set to `false`.
+/// * A [lightColorScheme] or [darkColorScheme] is consumed in Material Design 3. It only works if [material3] is set to `true`.
+///   System's current [ColorScheme] may be accessed using `package:dynamic_color`.
 ///
 class Visuals extends ChangeNotifier {
-  BuildContext context;
-  Color light;
-  Color dark;
+  /// Currently used Material Design standard e.g. Material Design 2 or Material Design 3.
+  int standard;
+
+  /// Current [ThemeMode] of the application.
   ThemeMode themeMode;
 
-  Visuals({
-    this.light = kPrimaryLightColor,
-    this.dark = kPrimaryDarkColor,
-    required this.themeMode,
-    required this.context,
-  }) {
-    update(context: context);
+  /// Whether system's current [ColorScheme] should be used or not.
+  bool systemColorScheme;
+
+  /// Current [BuildContext] of the application. Assigned at creation of widget tree.
+  BuildContext? context;
+
+  final Color? systemLight;
+  final Color? systemDark;
+  final ColorScheme? systemLightColorScheme;
+  final ColorScheme? systemDarkColorScheme;
+
+  /// Singleton instance of [Visuals]. Must call [initialize].
+  static late final Visuals instance;
+
+  /// Prevent calling [initialize] more than once.
+  static bool _initialized = false;
+
+  /// Initialize [Visuals] singleton instance.
+  static Future<void> initialize({
+    required int standard,
+    required ThemeMode themeMode,
+    required bool systemColorScheme,
+  }) async {
+    if (_initialized) {
+      return;
+    }
+    _initialized = true;
+
+    ColorScheme? systemLightColorScheme;
+    ColorScheme? systemDarkColorScheme;
+    await () async {
+      try {
+        // Android.
+        final corePalette = await DynamicColorPlugin.getCorePalette();
+        systemLightColorScheme =
+            corePalette?.toColorScheme(brightness: Brightness.light);
+        systemDarkColorScheme =
+            corePalette?.toColorScheme(brightness: Brightness.dark);
+        return;
+      } catch (exception, stacktrace) {
+        debugPrint(exception.toString());
+        debugPrint(stacktrace.toString());
+      }
+      try {
+        // Windows, Linux & macOS.
+        final accentColor = await DynamicColorPlugin.getAccentColor();
+        if (accentColor != null) {
+          systemLightColorScheme = ColorScheme.fromSeed(
+            seedColor: accentColor,
+            brightness: Brightness.light,
+          );
+          systemDarkColorScheme = ColorScheme.fromSeed(
+            seedColor: accentColor,
+            brightness: Brightness.dark,
+          );
+        }
+        return;
+      } catch (exception, stacktrace) {
+        debugPrint(exception.toString());
+        debugPrint(stacktrace.toString());
+      }
+    }();
+
+    instance = Visuals._(
+      standard: standard,
+      themeMode: themeMode,
+      systemColorScheme: systemColorScheme,
+      systemLight: systemLightColorScheme?.primary,
+      systemDark: systemDarkColorScheme?.primary,
+      systemLightColorScheme: systemLightColorScheme,
+      systemDarkColorScheme: systemDarkColorScheme,
+    );
   }
 
-  void update({
-    Color? light,
-    Color? dark,
+  Visuals._({
+    required this.standard,
+    required this.themeMode,
+    required this.systemColorScheme,
+    required this.systemLight,
+    required this.systemDark,
+    required this.systemLightColorScheme,
+    required this.systemDarkColorScheme,
+  });
+
+  Color get light {
+    if (systemColorScheme) {
+      return systemLight ?? kDefaultLightPrimaryColorM2;
+    }
+    return kDefaultLightPrimaryColorM2;
+  }
+
+  Color get dark {
+    if (systemColorScheme) {
+      return systemDark ?? kDefaultDarkPrimaryColorM2;
+    }
+    return kDefaultDarkPrimaryColorM2;
+  }
+
+  ColorScheme get lightColorScheme {
+    if (systemColorScheme) {
+      return systemLightColorScheme ?? defaultLightColorSchemeM3;
+    }
+    return defaultLightColorSchemeM3;
+  }
+
+  ColorScheme get darkColorScheme {
+    if (systemColorScheme) {
+      return systemDarkColorScheme ?? defaultDarkColorSchemeM3;
+    }
+    return defaultDarkColorSchemeM3;
+  }
+
+  ThemeData get adaptive => themeMode == ThemeMode.light ? theme : darkTheme;
+
+  ThemeData get theme => {
+        3: createM3Theme(
+          colorScheme: lightColorScheme,
+          mode: ThemeMode.light,
+        ),
+        2: createM2Theme(
+          color: light,
+          mode: ThemeMode.light,
+        )
+      }[standard]!;
+
+  ThemeData get darkTheme => {
+        3: createM3Theme(
+          colorScheme: darkColorScheme,
+          mode: ThemeMode.dark,
+        ),
+        2: createM2Theme(
+          color: dark,
+          mode: ThemeMode.dark,
+        )
+      }[standard]!;
+
+  Future<void> update({
     ThemeMode? themeMode,
-    TargetPlatform? platform,
+    int? standard,
     BuildContext? context,
   }) async {
-    this.light = light ?? this.light;
-    this.dark = dark ?? this.dark;
     this.themeMode = themeMode ?? this.themeMode;
-    if (isMobile && context != null) {
-      SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(
-          statusBarColor: Theme.of(context).brightness == Brightness.dark
-              ? Colors.black12
-              : Colors.white12,
-          statusBarIconBrightness:
-              Theme.of(context).brightness == Brightness.dark
-                  ? Brightness.light
-                  : Brightness.dark,
-        ),
-      );
+    this.standard = standard ?? this.standard;
+    this.context = context ?? this.context;
+    themeMode ??= this.themeMode;
+    standard ??= this.standard;
+    context ??= this.context;
+    debugPrint(themeMode.toString());
+    debugPrint(standard.toString());
+    if (context != null) {
+      if (Platform.isAndroid || Platform.isIOS) {
+        final brightness = Theme.of(context).brightness == Brightness.dark
+            ? Brightness.light
+            : Brightness.dark;
+        switch (standard) {
+          case 3:
+            SystemChrome.setSystemUIOverlayStyle(
+              SystemUiOverlayStyle(
+                statusBarBrightness: brightness,
+                statusBarIconBrightness: brightness,
+                statusBarColor: Colors.transparent,
+                // Edge-to-edge effect.
+                systemNavigationBarIconBrightness: brightness,
+                systemNavigationBarColor:
+                    adaptive.navigationBarTheme.backgroundColor,
+                systemNavigationBarDividerColor:
+                    adaptive.navigationBarTheme.backgroundColor,
+              ),
+            );
+            break;
+          case 2:
+            SystemChrome.setSystemUIOverlayStyle(
+              SystemUiOverlayStyle(
+                statusBarBrightness: brightness,
+                statusBarIconBrightness: brightness,
+                statusBarColor: Colors.transparent,
+                // Always stays black.
+                systemNavigationBarIconBrightness: Brightness.dark,
+                systemNavigationBarColor: Colors.black,
+                systemNavigationBarDividerColor: Colors.black,
+              ),
+            );
+            break;
+          default:
+            throw ArgumentError.value(
+              standard,
+              'Material Design standard',
+              'Only valid values are 2 & 3.',
+            );
+        }
+      }
+    } else {
+      debugPrint('No [BuildContext] provided.');
     }
-    this.notifyListeners();
-    await Configuration.instance.save(themeMode: this.themeMode);
+    notifyListeners();
+    await Configuration.instance.save(themeMode: themeMode);
   }
-
-  ThemeData get theme => createTheme(
-        color: light,
-        mode: ThemeMode.light,
-      );
-
-  ThemeData get darkTheme => createTheme(
-        color: dark,
-        mode: ThemeMode.dark,
-      );
 }
