17830a6ec33708e3b440c5ea4fcf9f97aebacbb8
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-02-28 02:11:08 +0530

    feat: LyricsApi

---
+++ lib/api/lyrics_api.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:harmonoid/models/lyric.dart';
+
+/// {@template lyrics_api}
+///
+/// LyricsApi
+/// ---------
+/// API to fetch lyrics.
+///
+/// {@endtemplate}
+class LyricsApi {
+  /// Singleton instance.
+  static const LyricsApi instance = LyricsApi._();
+
+  /// {@macro lyrics_api}
+  const LyricsApi._();
+
+  Future<List<Lyric>?> lyrics(String name) async {
+    try {
+      final response = await http.get(
+        Uri.https(
+          _base,
+          _lyrics,
+          {
+            'name': name,
+          },
+        ),
+      );
+      final body = json.decode(response.body);
+      return body.map<Lyric>((e) => Lyric.fromJson(e)).toList();
+    } catch (exception, stacktrace) {
+      debugPrint(exception.toString());
+      debugPrint(stacktrace.toString());
+      return null;
+    }
+  }
+
+  /// Base URL.
+  static const String _base = 'harmonoid-lyrics.vercel.app/api';
+
+  /// Endpoint: [lyrics].
+  static const String _lyrics = '/lyrics';
+}
