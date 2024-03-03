5557cb62a80bb5fc0200acee853c42dfbb61e9cd
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-03-03 17:51:30 +0530

    refactor: entry-point

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,25 @@
 import 'dart:io';
-import 'package:flutter/services.dart';
+import 'package:adaptive_layouts/adaptive_layouts.dart';
 import 'package:flutter/material.dart' hide Intent;
-import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:media_kit/media_kit.dart';
-import 'package:window_plus/window_plus.dart';
-import 'package:dart_discord_rpc/dart_discord_rpc.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:media_kit_tag_reader/media_kit_tag_reader.dart';
-import 'package:external_media_provider/external_media_provider.dart';
-import 'package:system_media_transport_controls/system_media_transport_controls.dart';
-
-import 'package:harmonoid/core/intent.dart';
-import 'package:harmonoid/core/playback.dart';
-import 'package:harmonoid/core/app_state.dart';
-import 'package:harmonoid/core/collection.dart';
-import 'package:harmonoid/core/configuration.dart';
-
-import 'package:harmonoid/state/lyrics.dart';
-import 'package:harmonoid/state/visuals.dart';
-import 'package:harmonoid/state/collection_refresh.dart';
-import 'package:harmonoid/state/now_playing_visuals.dart';
-
-import 'package:harmonoid/utils/window_lifecycle.dart';
-import 'package:harmonoid/utils/storage_retriever.dart';
-import 'package:harmonoid/utils/android_tag_reader.dart';
-
-import 'package:harmonoid/interface/harmonoid.dart';
-import 'package:harmonoid/interface/exception.dart';
 
 import 'package:harmonoid/constants/language.dart';
-
-const String kApplication = 'com.alexmercerind.harmonoid';
-const String kAuthor = 'Hitesh Kumar Saini <saini123hitesh@gmail.com>';
-const String kCaption = 'Harmonoid Music';
-const String kLicense = 'End-User License Agreement for Harmonoid';
-const String kTitle = 'Harmonoid';
-const String kVersion = 'v0.3.8';
+import 'package:harmonoid/core/configuration/configuration.dart';
+import 'package:harmonoid/core/intent.dart';
+import 'package:harmonoid/core/media_library.dart';
+import 'package:harmonoid/core/media_player.dart';
+import 'package:harmonoid/state/lyrics_notifier.dart';
+import 'package:harmonoid/state/now_playing_visuals_notifier.dart';
+import 'package:harmonoid/state/theme_notifier.dart';
+import 'package:harmonoid/ui/harmonoid.dart';
+import 'package:harmonoid/ui/exception.dart';
+import 'package:harmonoid/utils/android_storage_controller.dart';
+import 'package:harmonoid/utils/constants.dart';
+import 'package:harmonoid/utils/window_lifecycle.dart';
 
 Future<void> main(List<String> args) async {
-  try {
-    debugPrint(Platform.operatingSystemVersion);
-  } catch (exception, stacktrace) {
-    debugPrint(exception.toString());
-    debugPrint(stacktrace.toString());
-  }
   WidgetsFlutterBinding.ensureInitialized();
   try {
     HttpOverrides.global = _HttpOverrides();
@@ -52,145 +28,91 @@ Future<void> main(List<String> args) async {
     debugPrint(stacktrace.toString());
   }
   try {
-    if (Platform.isWindows) {
-      await WindowPlus.ensureInitialized(
-        application: kApplication,
-        enableEventStreams: false,
+    if (Platform.isAndroid) {
+      SystemChrome.setPreferredOrientations(
+        [
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ],
       );
-      WindowLifecycle.initialize();
-      MediaKit.ensureInitialized();
-      await Configuration.initialize();
-      await TagReader.initialize();
-      await ExternalMediaProvider.create();
-      await AppState.initialize();
-      await NowPlayingVisuals.initialize();
-      if (kReleaseMode || kProfileMode) {
-        await SystemMediaTransportControls.initialize();
+      await AndroidStorageController.ensureInitialized();
+      if (AndroidStorageController.instance.version >= 33) {
+        if (await Permission.audio.isDenied || await Permission.audio.isPermanentlyDenied) {
+          final state = await Permission.audio.request();
+          if (!state.isGranted) {
+            await SystemNavigator.pop();
+          }
+        }
+      } else {
+        if (await Permission.storage.isDenied || await Permission.storage.isPermanentlyDenied) {
+          final state = await Permission.storage.request();
+          if (!state.isGranted) {
+            await SystemNavigator.pop();
+          }
+        }
       }
-      await Intent.initialize(args: args);
-      await Visuals.initialize(
-        standard: 2,
-        themeMode: Configuration.instance.themeMode,
-        systemColorScheme: false,
-        animationDuration: Configuration.instance.animationDuration,
-      );
-      DiscordRPC.initialize();
     }
     if (Platform.isLinux) {
       await WindowPlus.ensureInitialized(
         application: kApplication,
         enableEventStreams: false,
       );
-      WindowLifecycle.initialize();
-      MediaKit.ensureInitialized();
-      await Configuration.initialize();
-      await TagReader.initialize();
-      await ExternalMediaProvider.create();
-      await AppState.initialize();
-      await NowPlayingVisuals.initialize();
-      await Intent.initialize(args: args);
-      await Visuals.initialize(
-        standard: 2,
-        themeMode: Configuration.instance.themeMode,
-        systemColorScheme: false,
-        animationDuration: Configuration.instance.animationDuration,
-      );
-      DiscordRPC.initialize();
+      await WindowPlus.instance.setMinimumSize(const Size(960.0, 640.0));
+      WindowLifecycle.ensureInitialized();
     }
-    if (Platform.isAndroid) {
-      await StorageRetriever.initialize();
-      SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
-      // Based on Material 2 specifications.
-      // Edge to edge based Material 3 configuration (if enabled by user) may be enabled during runtime later on.
-      SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(
-          systemNavigationBarColor: Colors.black,
-          systemNavigationBarDividerColor: Colors.black,
-          systemNavigationBarIconBrightness: Brightness.dark,
-        ),
+    if (Platform.isWindows) {
+      await WindowPlus.ensureInitialized(
+        application: kApplication,
+        enableEventStreams: false,
       );
-      // Android 12 or lower.
-      if (StorageRetriever.instance.version < 33) {
-        if (await Permission.storage.isDenied || await Permission.storage.isPermanentlyDenied) {
-          final state = await Permission.storage.request();
-          if (!state.isGranted) {
-            await SystemNavigator.pop(
-              animated: true,
-            );
-          }
-        }
-      }
-      // Android 13 or higher.
-      else {
-        if (await Permission.audio.isDenied || await Permission.audio.isPermanentlyDenied) {
-          final state = await Permission.audio.request();
-          if (!state.isGranted) {
-            await SystemNavigator.pop(
-              animated: true,
-            );
-          }
-        }
-      }
-      await Configuration.initialize();
-      await TagReader.initialize(
-        platformOverrides: {
-          Platform.operatingSystem: AndroidTagReader.instance,
-        },
-      );
-      await ExternalMediaProvider.create();
-      await AppState.initialize();
-      await Intent.initialize();
-      await Visuals.initialize(
-        standard: 2,
-        themeMode: Configuration.instance.themeMode,
-        systemColorScheme: false,
-        animationDuration: Configuration.instance.animationDuration,
-      );
+      await WindowPlus.instance.setMinimumSize(const Size(960.0, 640.0));
+      WindowLifecycle.ensureInitialized();
     }
-    await Collection.initialize(
-      collectionDirectories: Configuration.instance.collectionDirectories,
-      cacheDirectory: Configuration.instance.cacheDirectory,
-      albumsSort: Configuration.instance.albumsSort,
-      artistsSort: Configuration.instance.artistsSort,
-      tracksSort: Configuration.instance.tracksSort,
-      genresSort: Configuration.instance.genresSort,
-      albumsOrderType: Configuration.instance.albumsOrderType,
-      tracksOrderType: Configuration.instance.tracksOrderType,
-      artistsOrderType: Configuration.instance.artistsOrderType,
-      genresOrderType: Configuration.instance.genresOrderType,
-      minimumFileSize: Configuration.instance.minimumFileSize,
-      albumHashCodeParameters: Configuration.instance.albumHashCodeParameters,
+    MediaKit.ensureInitialized();
+    await Configuration.ensureInitialized();
+    await MediaLibrary.ensureInitialized(
+      cache: Configuration.instance.directory,
+      directories: Configuration.instance.mediaLibraryDirectories,
+      albumSortType: Configuration.instance.mediaLibraryAlbumSortType,
+      artistSortType: Configuration.instance.mediaLibraryArtistSortType,
+      genreSortType: Configuration.instance.mediaLibraryGenreSortType,
+      trackSortType: Configuration.instance.mediaLibraryTrackSortType,
+      albumSortAscending: Configuration.instance.mediaLibraryAlbumSortAscending,
+      artistSortAscending: Configuration.instance.mediaLibraryArtistSortAscending,
+      genreSortAscending: Configuration.instance.mediaLibraryGenreSortAscending,
+      trackSortAscending: Configuration.instance.mediaLibraryTrackSortAscending,
+      minimumFileSize: Configuration.instance.mediaLibraryMinimumFileSize,
+      albumGroupingParameters: Configuration.instance.mediaLibraryAlbumGroupingParameters,
     );
-    await Lyrics.initialize();
-    await Collection.instance.refresh(
-      onProgress: (progress, total, _) {
-        CollectionRefresh.instance.set(progress, total);
-      },
-      update: Configuration.instance.automaticMusicLookup,
+    await MediaPlayer.ensureInitialized();
+    await Intent.ensureInitialized(args: args);
+    await ThemeNotifier.ensureInitialized(
+      themeMode: Configuration.instance.themeMode,
+      materialVersion: Configuration.instance.themeMaterialVersion,
+      systemColorScheme: Configuration.instance.themeSystemColorScheme,
+      animationDuration: Configuration.instance.animationDuration,
     );
-    await Playback.initialize();
-    await Language.initialize(
+    await LyricsNotifier.ensureInitialized();
+    await NowPlayingVisualsNotifier.ensureInitialized();
+    await Language.ensureInitialized(
       language: Configuration.instance.language,
     );
-    runApp(Harmonoid());
+    runApp(const Harmonoid());
   } catch (exception, stacktrace) {
     debugPrint(exception.toString());
     debugPrint(stacktrace.toString());
     try {
-      WindowLifecycle.initialize();
+      await WindowPlus.ensureInitialized(
+        application: kApplication,
+        enableEventStreams: false,
+      );
+      await WindowPlus.instance.setMinimumSize(const Size(960.0, 640.0));
+      WindowLifecycle.ensureInitialized();
     } catch (exception, stacktrace) {
       debugPrint(exception.toString());
       debugPrint(stacktrace.toString());
     }
-    runApp(
-      ExceptionApp(
-        exception: exception,
-        stacktrace: stacktrace,
-      ),
-    );
+    runApp(ExceptionApp(exception: exception, stacktrace: stacktrace));
   }
 }
 
