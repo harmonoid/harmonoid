91aca7b0970602c3e6d84c1fb0b8e231a2b67b17
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-09-07 00:21:01 +0530

    feat: configuration_gen.key & configuration_keys.json

---
+++ scripts/configuration_gen.py
@@ -0,0 +1,88 @@
+import json
+
+
+def to_camel_case(snake_str):
+    components = snake_str.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
+
+
+def to_upper_camel_case(snake_str):
+    return "".join(x.title() for x in snake_str.split("_"))
+
+
+def generate_configuration(json_data):
+    class_content = []
+    getter_methods = []
+    set_method_content = []
+    get_defaults_method_content = []
+    private_fields = []
+    keys = []
+
+    for item in json_data:
+        key = item["key"]
+        dart_type = item["dartType"]
+        camel_case_key = to_camel_case(key.lower())
+        getter_methods.append(
+            f"  {dart_type} get {camel_case_key} => _{camel_case_key}!;"
+        )
+        set_method_content.append(
+            f"""    if ({camel_case_key} != null) {{
+      _{camel_case_key} = {camel_case_key};
+      await db.setValue(kKey{to_upper_camel_case(key)}, kType{item['serializedType']}, {item['serializedType'].lower()}Value: {f'{camel_case_key}.index' if item['serializedType'] == 'Integer' and item['dartType'] != 'int' else camel_case_key});
+    }}"""
+        )
+        get_defaults_method_content.append(
+            f"      /* {item['serializedType'].ljust(7)} */ kKey{to_upper_camel_case(key)}: {item['default']},"
+        )
+        private_fields.append(f"  {dart_type}? _{camel_case_key};")
+        keys.append(f"const kKey{to_upper_camel_case(key)} = '{key}';")
+    class_content = [
+        "// AUTO GENERATED FILE, DO NOT EDIT.",
+        "",
+        "part of 'configuration.dart';",
+        "",
+        "class ConfigurationBase {",
+        "",
+        "  final Directory directory;",
+        "  final Database db;",
+        "",
+        "  static bool get isMobile => Platform.isAndroid || Platform.isIOS;",
+        "  static bool get isDesktop => Platform.isLinux || Platform.isMacOS || Platform.isWindows;",
+        "",
+        "  ConfigurationBase({required this.directory, required this.db});",
+        "",
+        "\n".join(getter_methods),
+        "\n  Future<void> set({",
+        ",\n".join(
+            f"    {item['dartType']}? {to_camel_case(item['key'].lower())}"
+            for item in json_data
+        )
+        + ",",
+        "  }) async {",
+        "\n".join(set_method_content),
+        "  }",
+        "",
+        "\n  Future<Map<String, dynamic>> getDefaults() async {",
+        "    return {",
+        "\n".join(get_defaults_method_content),
+        "    };",
+        "  }",
+        "",
+        "\n".join(private_fields),
+        "}",
+        "",
+        "// ----- Keys -----",
+        "",
+        "\n".join(keys),
+        "",
+    ]
+
+    return "\n".join(class_content)
+
+
+if __name__ == "__main__":
+    input_file = "configuration_keys.json"
+    output_file = "../lib/core/configuration/configuration.g.dart"
+    input = open(input_file, "r")
+    output = open(output_file, "w")
+    output.write(generate_configuration(json.loads(input.read())))
