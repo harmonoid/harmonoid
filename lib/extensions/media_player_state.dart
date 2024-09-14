f168f19d7851ef6d1423f7856aae1fecc50c9f8b
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-09-14 23:24:29 +0530

    fix: getAudioFormatLabel

--- lib/extensions/media_player_state.dart
+++ lib/extensions/media_player_state.dart
@@ -1,5 +1,6 @@
 import 'package:harmonoid/models/media_player_state.dart';
 import 'package:harmonoid/models/playable.dart';
+import 'package:path/path.dart';
 
 /// Extensions for [MediaPlayerState].
 extension MediaPlayerStateExtensions on MediaPlayerState {
@@ -7,13 +8,10 @@ extension MediaPlayerStateExtensions on MediaPlayerState {
   bool get isEmpty => playables.isEmpty;
 
   /// Whether currently playing [Playable] is the first.
-  bool get isFirst => index == 0;
+  bool get isFirst => isEmpty || index == 0;
 
   /// Whether currently playing [Playable] is the last.
-  bool get isLast => index == playables.length - 1;
-
-  /// Currently playing [Playable].
-  Playable get current => playables[index];
+  bool get isLast => isEmpty || index == playables.length - 1;
 
   /// Audio format label.
   String getAudioFormatLabel({
@@ -23,11 +21,35 @@ extension MediaPlayerStateExtensions on MediaPlayerState {
     bool channelCount = true,
   }) {
     if (index < 0 || index > playables.length - 1) return '';
+
+    String formatValue = '';
+    String bitrateValue = '';
+    String sampleRateValue = '';
+    String channelCountValue = '';
+    try {
+      formatValue = extension(playables[index].uri).substring(1).toUpperCase();
+    } catch (_) {}
+    try {
+      if (audioBitrate > 0.0) {
+        bitrateValue = '${audioBitrate ~/ 1000} kb/s';
+      }
+    } catch (_) {}
+    try {
+      if (audioParams.sampleRate != null) {
+        sampleRateValue = '${(audioParams.sampleRate! / 1000).toStringAsFixed(1)} kHz';
+      }
+    } catch (_) {}
+    try {
+      if (audioParams.channelCount != null) {
+        channelCountValue = {1: 'Mono', 2: 'Stereo'}[audioParams.channelCount!] ?? '${audioParams.channelCount} channels';
+      }
+    } catch (_) {}
+
     return [
-      if (format && audioParams.format != null) audioParams.format!.toUpperCase(),
-      if (bitrate && audioBitrate > 0.0) '${audioBitrate ~/ 1000} kb/s',
-      if (sampleRate && audioParams.sampleRate != null) '${(audioParams.sampleRate! / 1000).toStringAsFixed(1)} kHz',
-      if (channelCount && audioParams.channelCount != null) {1: 'Mono', 2: 'Stereo'}[audioParams.channelCount!] ?? '${audioParams.channelCount} channels',
+      if (format && formatValue.isNotEmpty) formatValue,
+      if (bitrate && bitrateValue.isNotEmpty) bitrateValue,
+      if (sampleRate && sampleRateValue.isNotEmpty) sampleRateValue,
+      if (channelCount && channelCountValue.isNotEmpty) channelCountValue,
     ].join(' • ');
   }
 }
