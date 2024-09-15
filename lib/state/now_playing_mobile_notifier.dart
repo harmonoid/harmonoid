5b541e420e703abb9c5af4f752f8043399ef7d58
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-09-16 02:05:30 +0530

    refactor: MobileNowPlayingNotifier -> NowPlayingMobileNotifier

---
+++ lib/state/now_playing_mobile_notifier.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/widgets.dart';
+
+/// {@template now_playing_mobile_notifier}
+///
+/// NowPlayingMobileNotifier
+/// -------------------------
+/// Implementation to notify now playing bar & screen on mobile.
+///
+/// {@endtemplate}
+class NowPlayingMobileNotifier {
+  /// Singleton instance.
+  static final NowPlayingMobileNotifier instance = NowPlayingMobileNotifier._();
+
+  /// {@macro now_playing_mobile_notifier}
+  NowPlayingMobileNotifier._();
+
+  /// [GlobalKey] to access now playing bar.
+  // TODO:
+
+  /// Whether the [NowPlayingBarMobile] is in restored state.
+  // TODO:
+  final bool restored = true;
+
+  /// Offset for the bottom navigation bar & floating action button.
+  final ValueNotifier<double> bottomNavigationBarOffset = ValueNotifier<double>(0.0);
+
+  void show() {
+    // TODO:
+  }
+
+  void hide() {
+    // TODO:
+  }
+
+  void maximize() {
+    // TODO:
+  }
+
+  void restore() {
+    // TODO:
+  }
+}
