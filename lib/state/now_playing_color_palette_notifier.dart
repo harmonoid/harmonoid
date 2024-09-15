4fd16b34306bbf0147c4bd19e402ebfb1cb1e82e
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-09-16 02:07:32 +0530

    refactor: link NowPlayingColorPaletteNotifier to MediaPlayer internally

--- lib/state/now_playing_color_palette_notifier.dart
+++ lib/state/now_playing_color_palette_notifier.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'package:flutter/widgets.dart';
 import 'package:synchronized/synchronized.dart';
 
-import 'package:harmonoid/core/configuration/configuration.dart';
+import 'package:harmonoid/core/media_player.dart';
 import 'package:harmonoid/models/playable.dart';
 import 'package:harmonoid/utils/palette_generator.dart';
 import 'package:harmonoid/utils/rendering.dart';
@@ -18,46 +18,76 @@ class NowPlayingColorPaletteNotifier extends ChangeNotifier {
   /// Singleton instance.
   static final NowPlayingColorPaletteNotifier instance = NowPlayingColorPaletteNotifier._();
 
+  /// Whether the [instance] is initialized.
+  static bool initialized = false;
+
   /// {@macro now_playing_color_palette_notifier}
   NowPlayingColorPaletteNotifier._();
 
-  /// Current palette.
-  final ValueNotifier<List<Color>?> palette = ValueNotifier<List<Color>?>(null);
+  /// Initializes the [instance].
+  static Future<void> ensureInitialized() async {
+    if (initialized) return;
+    initialized = true;
+    try {
+      instance.update(MediaPlayer.instance.current);
+    } catch (exception, stacktrace) {
+      debugPrint(exception.toString());
+      debugPrint(stacktrace.toString());
+    }
+    MediaPlayer.instance.addListener(instance.listener);
+  }
 
-  /// Sets current [playable].
-  Future<void> update(
-    Playable playable, {
-    bool force = false,
-  }) {
-    return _lock.synchronized(() async {
-      if (Configuration.instance.desktopNowPlayingBarColorPalette) {
-        if (_current != playable || force) {
-          _current = playable;
-          try {
-            final image = cover(uri: playable.uri, cacheWidth: 20);
-            final result = await PaletteGenerator.fromImageProvider(image);
-            palette.value = result.colors?.toList();
-            notifyListeners();
-          } catch (exception, stacktrace) {
-            debugPrint(exception.toString());
-            debugPrint(stacktrace.toString());
-            palette.value = null;
-            notifyListeners();
-          }
-        }
+  /// Current color palette.
+  List<Color>? palette;
+
+  /// Listener to extract the color palette from current [Playable] in [MediaPlayer].
+  void listener() {
+    update(MediaPlayer.instance.current);
+  }
+
+  /// Updates the [palette] based on the specified [playable].
+  Future<void> update(Playable playable) async {
+    if (_current == playable) return;
+    _current = playable;
+    _updateInvoked = true;
+    return _updateLock.synchronized(() async {
+      _updateInvoked = false;
+      try {
+        final image = cover(uri: playable.uri, cacheWidth: 20);
+        final result = await PaletteGenerator.fromImageProvider(image);
+        // Return prematurely if the method has been invoked again.
+        if (_updateInvoked) return;
+        palette = result.colors?.toList();
+        notifyListeners();
+      } catch (exception, stacktrace) {
+        debugPrint(exception.toString());
+        debugPrint(stacktrace.toString());
+        palette = null;
+        notifyListeners();
       }
     });
   }
 
   /// Clears the currently extracted [palette] & notifies the listeners.
   void clear() {
-    palette.value = null;
+    palette = null;
+    _current = null;
     notifyListeners();
   }
 
-  /// Current playable.
+  /// Disposes the [instance]. Releases allocated resources back to the system.
+  @override
+  void dispose() {
+    super.dispose();
+    MediaPlayer.instance.removeListener(listener);
+  }
+
+  /// Current [Playable].
   Playable? _current;
 
-  /// Lock.
-  final Lock _lock = Lock();
+  /// Whether [update] has been invoked.
+  bool _updateInvoked = false;
+
+  /// Mutual exclusion in [update] invocations.
+  final Lock _updateLock = Lock();
 }
