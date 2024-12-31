ac12fed059691ef3037f157c6f7246b5263e2ac3
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-12-31 21:54:07 +0530

    refactor(android): re-write intent & storage controllers

--- android/app/src/main/kotlin/com/alexmercerind/harmonoid/MainActivity.kt
+++ android/app/src/main/kotlin/com/alexmercerind/harmonoid/MainActivity.kt
@@ -2,89 +2,59 @@ package com.alexmercerind.harmonoid
 
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.content.pm.ResolveInfo
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.ryanheise.audioservice.AudioServiceActivity
 import io.flutter.Log
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
-import java.util.Random
 import java.io.File
 import java.io.FileOutputStream
-import java.io.InputStream
-import com.ryanheise.audioservice.AudioServiceActivity
-import java.math.BigInteger
 import java.security.MessageDigest
-
-private const val METADATA_RETRIEVER_CHANNEL_NAME: String =
-    "com.alexmercerind.harmonoid.MetadataRetriever"
-private const val STORAGE_RETRIEVER_CHANNEL_NAME: String =
-    "com.alexmercerind.harmonoid.StorageRetriever"
-private const val INTENT_RETRIEVER_CHANNEL_NAME: String =
-    "com.alexmercerind.harmonoid.IntentRetriever"
+import kotlin.io.path.Path
 
 class MainActivity : AudioServiceActivity() {
-    private var metadataRetrieverChannel: MethodChannel? = null
-    private var storageRetrieverChannel: MethodChannel? = null
-    private var intentRetrieverChannel: MethodChannel? = null
+    companion object {
+        private const val TAG = "Harmonoid"
+
+        private const val INTENT_CONTROLLER_CHANNEL_NAME: String = "com.alexmercerind.harmonoid/intent_controller"
+        private const val STORAGE_CONTROLLER_CHANNEL_NAME: String = "com.alexmercerind.harmonoid/storage_controller"
+
+        private const val NOTIFY_INTENT_METHOD_NAME = "notifyIntent"
+
+        private const val SCHEME_CONTENT = "content"
+        private const val SCHEME_FILE = "file"
+        private const val SCHEME_HTTP = "http"
+        private const val SCHEME_HTTPS = "https"
+    }
+
+    private var intentControllerMethodChannel: MethodChannel? = null
+    private var storageControllerMethodChannel: MethodChannel? = null
     private var uri: String? = null
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
-        Log.d(
-            "Harmonoid",
-            context.getExternalFilesDirs(null).map { file -> file.absolutePath }.toString()
-        )
-        metadataRetrieverChannel = MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            METADATA_RETRIEVER_CHANNEL_NAME
-        )
-        storageRetrieverChannel = MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            STORAGE_RETRIEVER_CHANNEL_NAME
-        )
-        intentRetrieverChannel = MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            INTENT_RETRIEVER_CHANNEL_NAME
-        )
-        metadataRetrieverChannel?.setMethodCallHandler(MetadataRetriever())
-        storageRetrieverChannel?.setMethodCallHandler(
-            StorageRetriever(
-                storageRetrieverChannel,
-                context
-            )
-        )
-        intentRetrieverChannel?.setMethodCallHandler { _, result ->
-            result.success(uri)
+        Log.d(TAG, context.getExternalFilesDirs(null).map { file -> file.absolutePath }.toString())
+
+        intentControllerMethodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, INTENT_CONTROLLER_CHANNEL_NAME).apply {
+            setMethodCallHandler { _, result -> result.success(uri) }
+        }
+        storageControllerMethodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, STORAGE_CONTROLLER_CHANNEL_NAME).apply {
+            setMethodCallHandler(StorageControllerMethodCallHandler(this@MainActivity, this))
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        // From [StorageRetriever] class.
         when (requestCode) {
-            STORAGE_RETRIEVER_DELETE_REQUEST_CODE -> {
-                // Android 10 is retarded. Sorry, all Android versions after 9 are retarded.
-                // [File] needs to be deleted afterwards here.
+            StorageControllerMethodCallHandler.DELETE_REQUEST_CODE -> {
                 if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q && resultCode == RESULT_OK && data != null) {
-                    try {
-                        context.contentResolver.delete(
-                            Uri.parse(data.action),
-                            null,
-                            null
-                        )
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
+                    runCatching { context.contentResolver.delete(Uri.parse(data.action), null, null) }
                 }
-                Log.d("Harmonoid", "STORAGE_RETRIEVER_DELETE_NOTIFY_METHOD_NAME: $resultCode")
-                storageRetrieverChannel?.invokeMethod(
-                    STORAGE_RETRIEVER_DELETE_NOTIFY_METHOD_NAME,
-                    resultCode == RESULT_OK
-                )
+                storageControllerMethodChannel?.invokeMethod(StorageControllerMethodCallHandler.NOTIFY_DELETE_METHOD_NAME, resultCode == RESULT_OK)
             }
         }
     }
@@ -107,113 +77,45 @@ class MainActivity : AudioServiceActivity() {
 
     @Synchronized
     private fun receiveIntent(intent: Intent?) {
-        // This has been configured to work correctly with android:launchMode as singleTask.
-        if (intent != null) {
-            var result: String? = null
-            Log.d("Harmonoid", intent.scheme.toString())
-            Log.d("Harmonoid", intent.action.toString())
-            Log.d("Harmonoid", intent.type.toString())
-            Log.d("Harmonoid", intent.data.toString())
-            if (arrayListOf("file", "http").contains(intent.data?.scheme)) {
-                result = intent.data.toString()
-            }
-            // Separately handle modern content:// URIs in Android.
-            else if (intent.data?.scheme == "content") {
-                val resolveInfoList: List<ResolveInfo> = context.packageManager
-                    .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
-                // TODO: [queryIntentActivities] with [ResolveInfoFlags] overload seems to be not working on Android 13.
-                // val resolveInfoList: List<ResolveInfo> =
-                //     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                //         context.packageManager
-                //             .queryIntentActivities(
-                //                 intent,
-                //                 PackageManager.ResolveInfoFlags.of(PackageManager.MATCH_DEFAULT_ONLY.toLong())
-                //             )
-                //     } else {
-                //         context.packageManager
-                //             .queryIntentActivities(
-                //                 intent,
-                //                 PackageManager.MATCH_DEFAULT_ONLY
-                //             )
-                //     }
-                // Not requesting Intent.FLAG_GRANT_READ_URI_PERMISSION.
-                // Since, we are never going to write the file which is being opened.
-                //
-                // It requires different approach in new Android versions to write the opened file.
-                // Likely through ACTION_OPEN_DOCUMENT & FileDescriptor.
-                // Though, since this seems to work perfectly. It's good to go.
-                for (resolveInfo in resolveInfoList) {
-                    val packageName: String = resolveInfo.activityInfo.packageName
-                    context.grantUriPermission(
-                        packageName,
-                        intent.data, Intent.FLAG_GRANT_READ_URI_PERMISSION
-                    )
+        if (intent == null) return
+        Log.d(TAG, intent.action.toString())
+        Log.d(TAG, intent.data.toString())
+        Log.d(TAG, intent.scheme.toString())
+        Log.d(TAG, intent.type.toString())
+        val result = when (intent.data?.scheme?.lowercase()) {
+            SCHEME_CONTENT -> {
+                val resolveInfos = context.packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+                for (resolveInfo in resolveInfos) {
+                    val packageName = resolveInfo.activityInfo.packageName
+                    context.grantUriPermission(packageName, intent.data, Intent.FLAG_GRANT_READ_URI_PERMISSION)
                 }
-                // In general, the idea is to copy the file to a local temporary file with unique URI received through intent, inside app's cache directory.
-                //
-                // Get reference to the InputStream from this content:// URI.
-                val inputStream: InputStream? = contentResolver.openInputStream(intent.data!!)
-                // The cache directory of the application. Equivalent to getExternalStorageDirectory in package:path_provider of Flutter.
-                var externalFilesDirAbsolutePath = context.getExternalFilesDir(null)?.absolutePath!!
-                if (externalFilesDirAbsolutePath.endsWith("/")) {
-                    externalFilesDirAbsolutePath = externalFilesDirAbsolutePath.substring(
-                        0, externalFilesDirAbsolutePath.length - 1
-                    )
-                }
-                // Saving the file in "Intents" subdirectory, for easy clean-up the cache in future.
-                val intentFilesDirAbsolutePath = "$externalFilesDirAbsolutePath/Intents"
-                Log.d("Harmonoid", intentFilesDirAbsolutePath)
-                // Last segment of the URI is interpreted as the file path.
-                // Removing all special characters to prevent any issues with URI deserialization/serialization inside Flutter or file creation.
-                val fileName = intent.data.toString().split("/").toList().lastOrNull()
-                    ?.replace("[^a-zA-Z0-9]".toRegex(), "")
-                Log.d("Harmonoid", fileName.toString())
-                // If file name is null, then a random integer value is used as the temporary file's name.
-                // Hashing the resulting [fileName] as MD5. This will prevent any file name too long [IOException]s.
-                val path = "$intentFilesDirAbsolutePath/${getFileNameIdentifier(fileName)}"
-                result = "file://$path"
-                Log.d("Harmonoid", path)
-                // Only create/copy the content from the [Intent] if same file does not exist.
-                if (!File(path).exists()) {
-                    Log.d("Harmonoid", path)
-                    // Delete the directory where the temporary files are placed. This is because some previous intent handling would've resulted in file creations here.
-                    // This wastage of space can quickly get out of hands.
-                    if (File(intentFilesDirAbsolutePath).exists()
-                        && File(intentFilesDirAbsolutePath).isDirectory
-                    ) {
-                        File(intentFilesDirAbsolutePath).deleteRecursively()
+                contentResolver.openInputStream(intent.data!!).use { stream ->
+                    val file = File(Path(context.getExternalFilesDir(null)!!.absolutePath, "Intent", intent.data.toString().md5()).toString())
+                    if (!file.exists()) {
+                        if (file.parentFile?.exists() == true) {
+                            file.parentFile?.deleteRecursively()
+                        }
+                        file.parentFile?.mkdirs()
+                        file.createNewFile()
+                        stream?.copyTo(FileOutputStream(file))
                     }
-                    // Recursively create all the directories & subdirectories to the temporary file, and copy the stream to it after creation of file itself.
-                    File(intentFilesDirAbsolutePath).mkdirs()
-                    if (!File(path).exists()) {
-                        File(path).createNewFile()
-                    }
-                    inputStream?.copyTo(FileOutputStream(path))
+                    "$SCHEME_FILE://${file.absolutePath}"
                 }
             }
-            if (!arrayListOf(null, uri).contains(result)) {
-                // Notify the newly opened file through the platform channel.
-                uri = result
-                intentRetrieverChannel?.invokeMethod("Harmonoid", uri)
-                Log.d("Harmonoid", uri.toString())
-            }
+
+            SCHEME_FILE, SCHEME_HTTP, SCHEME_HTTPS -> intent.data.toString()
+
+            else -> null
+        }
+
+        Log.d(TAG, uri.toString())
+
+        if (result != null && result != uri) {
+            uri = result
+            intentControllerMethodChannel?.invokeMethod(NOTIFY_INTENT_METHOD_NAME, uri)
         }
     }
 
-    private fun getFileNameIdentifier(input: String?): String {
-        val instance = MessageDigest.getInstance("MD5")
-        return if (input != null) {
-            val result =
-                BigInteger(
-                    1,
-                    instance.digest(input.toByteArray())
-                ).toString(16).padStart(32, '0')
-            val identifier = result.replace("[^a-zA-Z0-9]".toRegex(), "")
-            Log.d("Harmonoid", identifier)
-            identifier
-        } else {
-            // Shouldn't ever happen, but still present as a fallback.
-            Random().nextInt(Integer.MAX_VALUE).toString()
-        }
-    }
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun String.md5() = MessageDigest.getInstance("MD5").digest(this.toByteArray()).toHexString()
 }
---
+++ android/app/src/main/kotlin/com/alexmercerind/harmonoid/StorageControllerMethodCallHandler.kt
@@ -0,0 +1,223 @@
+package com.alexmercerind.harmonoid
+
+import android.app.Activity
+import android.app.RecoverableSecurityException
+import android.content.ContentUris
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+import io.flutter.Log
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import java.io.File
+
+class StorageControllerMethodCallHandler(private val activity: Activity, private val channel: MethodChannel) : MethodChannel.MethodCallHandler {
+    companion object {
+        private const val TAG = "Harmonoid"
+
+        private const val GET_STORAGE_DIRECTORIES_METHOD_NAME = "getStorageDirectories"
+        private const val GET_CACHE_DIRECTORY_METHOD_NAME = "getCacheDirectory"
+        private const val GET_VERSION_METHOD_NAME = "getVersion"
+        private const val DELETE_METHOD_NAME = "delete"
+        /* private */ const val NOTIFY_DELETE_METHOD_NAME = "notifyDelete"
+
+        private const val DELETE_ARG_PATHS = "paths"
+
+        /* private */ const val DELETE_REQUEST_CODE = 1
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            GET_STORAGE_DIRECTORIES_METHOD_NAME -> {
+                val value = activity.getExternalFilesDirs(null).map { file -> file.absolutePath.split("/Android/")[0] }
+                Log.d(TAG, value.toString())
+                result.success(value)
+            }
+
+            GET_CACHE_DIRECTORY_METHOD_NAME -> {
+                val value = activity.getExternalFilesDirs(null).first().absolutePath
+                Log.d(TAG, value.toString())
+                result.success(value)
+            }
+
+            GET_VERSION_METHOD_NAME -> {
+                val version: Int = Build.VERSION.SDK_INT
+                Log.d(TAG, version.toString())
+                result.success(version)
+            }
+
+            DELETE_METHOD_NAME -> {
+                val paths = call.argument<List<String>>(DELETE_ARG_PATHS)
+                if (!paths.isNullOrEmpty()) {
+                    when {
+                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> deleteAPILevel30(paths)
+                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> deleteAPILevel29(paths)
+                        else -> deleteAPILevel28(paths)
+                    }
+                }
+                result.success(null)
+            }
+
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun deleteAPILevel30(paths: List<String>) {
+        if (tryAndDelete(paths)) {
+            channel.invokeMethod(NOTIFY_DELETE_METHOD_NAME, true)
+            return
+        }
+
+        runCatching {
+            val pendingIntent = MediaStore.createDeleteRequest(activity.contentResolver, paths.map { path -> path.toContentUri()!! })
+            activity.startIntentSenderForResult(pendingIntent.intentSender, DELETE_REQUEST_CODE, null, 0, 0, 0, null)
+            // Refer MainActivity.onActivityResult for result handling.
+        }.onFailure {
+            it.printStackTrace()
+            channel.invokeMethod(NOTIFY_DELETE_METHOD_NAME, false)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun deleteAPILevel29(paths: List<String>) {
+        if (tryAndDelete(paths)) {
+            channel.invokeMethod(NOTIFY_DELETE_METHOD_NAME, true)
+            return
+        }
+
+        if (paths.size == 1) {
+            try {
+                val response = activity.contentResolver.delete(paths.first().toContentUri()!!, null, null)
+                channel.invokeMethod(NOTIFY_DELETE_METHOD_NAME, response > 0)
+            } catch (e: SecurityException) {
+                e.printStackTrace()
+                try {
+                    val recoverableSecurityException = e as? RecoverableSecurityException
+                    val intentSender = recoverableSecurityException?.userAction?.actionIntent?.intentSender
+                    if (intentSender != null) {
+                        activity.startIntentSenderForResult(
+                            intentSender, DELETE_REQUEST_CODE, Intent(paths.first().toContentUri().toString()), 0, 0, 0, null
+                        )
+                        // Refer MainActivity.onActivityResult for result handling.
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    channel.invokeMethod(NOTIFY_DELETE_METHOD_NAME, false)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                channel.invokeMethod(NOTIFY_DELETE_METHOD_NAME, false)
+            }
+        } else {
+            // API 29 can't delete multiple files at once.
+            channel.invokeMethod(NOTIFY_DELETE_METHOD_NAME, false)
+        }
+    }
+
+    private fun deleteAPILevel28(paths: List<String>) {
+        val result = tryAndDelete(paths)
+        channel.invokeMethod(NOTIFY_DELETE_METHOD_NAME, result)
+    }
+
+    private fun tryAndDelete(paths: List<String>): Boolean {
+        for (path in paths) {
+            val result = runCatching { File(path).deleteRecursively() }.getOrDefault(false)
+            if (!result) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun String.toContentUri(): Uri? {
+        val value = this
+
+        runCatching {
+            val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+            } else {
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+            }
+            val projection = arrayOf(MediaStore.Audio.Media._ID, MediaStore.Audio.Media.DATA)
+            val selection = "${MediaStore.Audio.Media.DATA} = ?"
+            val selectionArgs = arrayOf(value)
+            val sortOrder = "${MediaStore.Audio.Media._ID} ASC"
+            val query = activity.contentResolver.query(
+                collection, projection, selection, selectionArgs, sortOrder
+            )
+            query?.use { cursor ->
+                Log.d(TAG, "ContentResolver.query: cursor.count = ${cursor.count}")
+                if (cursor.count > 0) {
+                    cursor.moveToFirst()
+                    val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+                    val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)
+                    val id = cursor.getLong(idColumn)
+                    val data = cursor.getString(dataColumn)
+                    Log.d(TAG, "ContentResolver.query: id = $id")
+                    Log.d(TAG, "ContentResolver.query: data = $data")
+                    return ContentUris.withAppendedId(collection, id)
+                }
+            }
+        }
+        runCatching {
+            val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+            } else {
+                MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+            }
+            val projection = arrayOf(MediaStore.Video.Media._ID, MediaStore.Video.Media.DATA)
+            val selection = "${MediaStore.Video.Media.DATA} = ?"
+            val selectionArgs = arrayOf(value)
+            val sortOrder = "${MediaStore.Video.Media._ID} ASC"
+            val query = activity.contentResolver.query(
+                collection, projection, selection, selectionArgs, sortOrder
+            )
+            query?.use { cursor ->
+                Log.d(TAG, "ContentResolver.query: cursor.count = ${cursor.count}")
+                if (cursor.count > 0) {
+                    cursor.moveToFirst()
+                    val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
+                    val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA)
+                    val id = cursor.getLong(idColumn)
+                    val data = cursor.getString(dataColumn)
+                    Log.d(TAG, "ContentResolver.query: id = $id")
+                    Log.d(TAG, "ContentResolver.query: data = $data")
+                    return ContentUris.withAppendedId(collection, id)
+                }
+            }
+        }
+        runCatching {
+            val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+            } else {
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            }
+            val projection = arrayOf(MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA)
+            val selection = "${MediaStore.Images.Media.DATA} = ?"
+            val selectionArgs = arrayOf(value)
+            val sortOrder = "${MediaStore.Images.Media._ID} ASC"
+            val query = activity.contentResolver.query(
+                collection, projection, selection, selectionArgs, sortOrder
+            )
+            query?.use { cursor ->
+                Log.d(TAG, "ContentResolver.query: cursor.count = ${cursor.count}")
+                if (cursor.count > 0) {
+                    cursor.moveToFirst()
+                    val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                    val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                    val id = cursor.getLong(idColumn)
+                    val data = cursor.getString(dataColumn)
+                    Log.d(TAG, "ContentResolver.query: id = $id")
+                    Log.d(TAG, "ContentResolver.query: data = $data")
+                    return ContentUris.withAppendedId(collection, id)
+                }
+            }
+        }
+        return null
+    }
+}
