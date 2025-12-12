a86327b0c607e02e60e7a19823fc0c808c601769
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2025-12-13 00:44:37 +0530

    fix

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Flutter"
+export "FLUTTER_APPLICATION_PATH=D:\harmonoid"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.3.17"
+export "FLUTTER_BUILD_NUMBER=9028"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
