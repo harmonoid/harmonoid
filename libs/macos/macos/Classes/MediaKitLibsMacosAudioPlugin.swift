327267dfd62b36f727cc98ef728febd4c4ded1e0
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2025-12-14 00:08:40 +0530

    build(windows,linux): arm64 support

--- libs/macos/macos/Classes/MediaKitLibsMacosAudioPlugin.swift
+++
@@ -1,7 +0,0 @@
-import Cocoa
-import FlutterMacOS
-
-public class MediaKitLibsMacosAudioPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-  }
-}
