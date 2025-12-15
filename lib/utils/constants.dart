443624a1251867e3fb7d1c95a5ab06aafb2a91b0
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2025-12-16 02:20:43 +0530

    build: bump version to v0.3.18

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -60,4 +60,4 @@ const String kAuthor = 'Hitesh Kumar Saini';
 const String kCaption = 'Harmonoid Music';
 const String kLicense = 'PolyForm Strict License 1.0.0';
 const String kTitle = 'Harmonoid';
-const String kVersion = 'v0.3.17';
+const String kVersion = 'v0.3.18';
