a6b7cb73335f34503060354ac9585169113d30d9
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2025-12-16 14:07:06 +0530

    fix(android): sdk 21 support

--- android/app/src/main/kotlin/com/alexmercerind/harmonoid/Mappers.kt
+++ android/app/src/main/kotlin/com/alexmercerind/harmonoid/Mappers.kt
@@ -2,14 +2,14 @@ package com.alexmercerind.harmonoid
 
 import android.app.Activity
 import android.os.Environment
+import java.io.File
 import java.security.MessageDigest
-import kotlin.io.path.Path
 
-val Activity.storageDirectories: List<String> get() = getExternalFilesDirs(null).map { file -> file.absolutePath.split("/Android/")[0] }
+val Activity.storageDirectories: List<String> get() = getExternalFilesDirs(null).map { file -> file.absolutePath.split("/Android/").first() }
 
 val Activity.cacheDirectory: String get() = getExternalFilesDirs(null).first().absolutePath
 
-val Activity.defaultMediaLibraryDirectory get() = Path(getExternalFilesDirs(null).first().absolutePath.split("/Android/")[0], Environment.DIRECTORY_MUSIC).toString()
+val Activity.defaultMediaLibraryDirectory: String get() = File(getExternalFilesDirs(null).first().absolutePath.split("/Android/").first(), Environment.DIRECTORY_MUSIC).absolutePath
 
 @OptIn(ExperimentalStdlibApi::class)
 val String.md5 get() = MessageDigest.getInstance("MD5").digest(this.toByteArray()).toHexString()
