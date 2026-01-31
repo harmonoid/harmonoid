d5a3888e280e4fa33e9a98d7148edaf917e5e54d
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2026-01-31 14:37:14 +0530

    fix(win32): flutter::UIThreadPolicy::RunOnSeparateThread

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -15,6 +15,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
   flutter::DartProject project(L"data");
   std::vector<std::string> command_line_arguments = GetCommandLineArguments();
+  project.set_ui_thread_policy(flutter::UIThreadPolicy::RunOnSeparateThread);
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
