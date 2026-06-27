e635c95d06c62e755aa8c8d731c9cf456f701ce9
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2026-06-27 19:30:22 +0530

    fix(ios): notification action

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,7 @@ import MobileCoreServices
 import UIKit
 import UserNotifications
 import UniformTypeIdentifiers
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate, UIDocumentPickerDelegate {
@@ -33,13 +34,14 @@ import UniformTypeIdentifiers
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-        }
+        UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
     func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+        FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+            GeneratedPluginRegistrant.register(with: registry)
+        }
         GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
         
         intentControllerMethodChannel = FlutterMethodChannel(
