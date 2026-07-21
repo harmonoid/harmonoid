eba15b2e7a0c75d69754ec84b610c0d1c4e6edaf
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2026-07-22 03:23:17 +0530

    build(deps): bump version to 0.3.31+9042

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,4 +3,4 @@ const String kAuthor = 'Hitesh Kumar Saini';
 const String kCaption = 'Harmonoid Music';
 const String kLicense = 'PolyForm Strict License 1.0.0';
 const String kTitle = 'Harmonoid';
-const String kVersion = 'v0.3.30';
+const String kVersion = 'v0.3.31';
--- linux/rpm/harmonoid.spec
+++ linux/rpm/harmonoid.spec
@@ -1,5 +1,5 @@
 Name:       harmonoid
-Version:    0.3.30
+Version:    0.3.31
 Release:    1
 Summary:    Plays & manages your music library. Looks beautiful & juicy.
 License:    EULA
